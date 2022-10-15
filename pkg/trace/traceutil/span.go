a2dc32d02953f4ad6fc6e09ef4eb4bc384e39165
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:27:12 -0400

    Initialize gbbr/v1

---
+++ pkg/trace/traceutil/span.go
@@ -0,0 +1,76 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2020 Datadog, Inc.
+
+package traceutil
+
+import (
+	"strings"
+
+	"github.com/DataDog/datadog-agent/pkg/tagger"
+	"github.com/DataDog/datadog-agent/pkg/tagger/collectors"
+	"github.com/DataDog/datadog-agent/pkg/trace/pb"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+)
+
+const (
+	// TraceMetricsKey is a tag key which, if set to true,
+	// ensures all statistics are computed for this span.
+	// [FIXME] *not implemented yet*
+	TraceMetricsKey = "datadog.trace_metrics"
+
+	// This is a special metric, it's 1 if the span is top-level, 0 if not.
+	topLevelKey = "_top_level"
+)
+
+// HasTopLevel returns true if span is top-level.
+func HasTopLevel(s *pb.Span) bool {
+	return s.Metrics[topLevelKey] == 1
+}
+
+// HasForceMetrics returns true if statistics computation should be forced for this span.
+func HasForceMetrics(s *pb.Span) bool {
+	return s.Meta[TraceMetricsKey] == "true"
+}
+
+// SetTopLevel sets the top-level attribute of the span.
+func SetTopLevel(s *pb.Span, topLevel bool) {
+	if !topLevel {
+		if s.Metrics == nil {
+			return
+		}
+		delete(s.Metrics, topLevelKey)
+		return
+	}
+	// Setting the metrics value, so that code downstream in the pipeline
+	// can identify this as top-level without recomputing everything.
+	setMetric(s, topLevelKey, 1)
+}
+
+func setMetric(s *pb.Span, key string, val float64) {
+	if s.Metrics == nil {
+		s.Metrics = make(map[string]float64)
+	}
+	s.Metrics[key] = val
+}
+
+// GetContainerTags returns container and orchestrator tags belonging to containerID. If containerID
+// is empty or no tags are found, an empty string is returned.
+func GetContainerTags(containerID string) string {
+	list, err := tagger.Tag("container_id://"+containerID, collectors.HighCardinality)
+	if err != nil {
+		log.Tracef("Getting container tags for ID %q: %v", containerID, err)
+		return ""
+	}
+	log.Tracef("Getting container tags for ID %q: %v", containerID, list)
+	return strings.Join(list, ",")
+}
+
+// SetMeta sets the metadata at key to the val on the span s.
+func SetMeta(s *pb.Span, key, val string) {
+	if s.Meta == nil {
+		s.Meta = make(map[string]string)
+	}
+	s.Meta[key] = val
+}
