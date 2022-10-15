59d1a3bdbd862d71fb07b2afbab25414bf3c12ee
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 22:36:08 -0400

    Initialize julien.lebot/add_ca_unit_test_to_ci

---
+++ tools/windows/install-help/cal/customaction-tests/ReplaceYamlProperties.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "stdafx.h"
+#include "../PropertyReplacer.h"
+
+#include "gtest/gtest.h"
+
+class ReplaceYamlPropertiesTests : public testing::Test
+{
+};
+
+typedef std::map<std::wstring, std::wstring> value_map;
+property_retriever propertyRetriever(value_map const &values);
