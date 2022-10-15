9c0f1294823d662367dbd00a6b723fc26e106fc4
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:03:33 -0400

    Initialize jaime/addecouple

---
+++ pkg/util/compression/zstd.go
@@ -0,0 +1,11 @@
+// +build zstd
+// +build !zlib
+
+package compression
+
+import "github.com/DataDog/zstd"
+
+// Compress will compress the data with zstd
+func Compress(dst []byte, src []byte) ([]byte, error) {
+	return zstd.Compress(dst, src)
+}
