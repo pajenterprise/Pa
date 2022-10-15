bf20410131525fd04240345b4b80534e1d34bce5
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:08:39 -0400

    Initialize xvello/group-series

---
+++ pkg/metrics/series.go
@@ -0,0 +1,382 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2018 Datadog, Inc.
+
+package metrics
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"expvar"
+	"fmt"
+	"sort"
+	"strings"
+	"unsafe"
+
+	agentpayload "github.com/DataDog/agent-payload/gogen"
+	"github.com/gogo/protobuf/proto"
+	jsoniter "github.com/json-iterator/go"
+
+	"github.com/DataDog/datadog-agent/pkg/aggregator/ckey"
+	"github.com/DataDog/datadog-agent/pkg/serializer/marshaler"
+)
+
+var seriesExpvar = expvar.NewMap("series")
+
+var marshaller = jsoniter.Config{
+	EscapeHTML:                    false,
+	ObjectFieldMustBeSimpleString: true,
+}.Froze()
+
+var (
+	jsonSeparator  = []byte(",")
+	jsonArrayStart = []byte("[")
+	jsonArrayEnd   = []byte("]")
+)
+
+// Point represents a metric value at a specific time
+type Point struct {
+	Ts    float64
+	Value float64
+}
+
+// MarshalJSON return a Point as an array of value (to be compatible with v1 API)
+// FIXME(maxime): to be removed when v2 endpoints are available
+// Note: it is not used with jsoniter, encodePoints takes over
+func (p *Point) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("[%v, %v]", int64(p.Ts), p.Value)), nil
+}
+
+// Serie holds a timeseries (w/ json serialization to DD API format)
+type Serie struct {
+	Name           string          `json:"metric"`
+	Points         []Point         `json:"points"`
+	Tags           []string        `json:"tags"`
+	Host           string          `json:"host"`
+	Device         string          `json:"device,omitempty"` // FIXME(olivier): remove as soon as the v1 API can handle `device` as a regular tag
+	MType          APIMetricType   `json:"type"`
+	Interval       int64           `json:"interval"`
+	SourceTypeName string          `json:"source_type_name,omitempty"`
+	ContextKey     ckey.ContextKey `json:"-"`
+	NameSuffix     string          `json:"-"`
+}
+
+// Series represents a list of Serie ready to be serialize
+type Series []*Serie
+
+func marshalPoints(points []Point) []*agentpayload.MetricsPayload_Sample_Point {
+	pointsPayload := []*agentpayload.MetricsPayload_Sample_Point{}
+
+	for _, p := range points {
+		pointsPayload = append(pointsPayload, &agentpayload.MetricsPayload_Sample_Point{
+			Ts:    int64(p.Ts),
+			Value: p.Value,
+		})
+	}
+	return pointsPayload
+}
+
+// Marshal serialize timeseries using agent-payload definition
+func (series Series) Marshal() ([]byte, error) {
+	payload := &agentpayload.MetricsPayload{
+		Samples:  []*agentpayload.MetricsPayload_Sample{},
+		Metadata: &agentpayload.CommonMetadata{},
+	}
+
+	for _, serie := range series {
+		payload.Samples = append(payload.Samples,
+			&agentpayload.MetricsPayload_Sample{
+				Metric:         serie.Name,
+				Type:           serie.MType.String(),
+				Host:           serie.Host,
+				Points:         marshalPoints(serie.Points),
+				Tags:           serie.Tags,
+				SourceTypeName: serie.SourceTypeName,
+			})
+	}
+
+	return proto.Marshal(payload)
+}
+
+// populateDeviceField removes any `device:` tag in the serie tags and uses the value to
+// populate the Serie.Device field
+// Mutates the `serie` struct in place
+//FIXME(olivier): remove this as soon as the v1 API can handle `device` as a regular tag
+func populateDeviceField(serie *Serie) {
+	// make a copy of the tags array. Otherwise the underlying array won't have
+	// the device tag for the Nth iteration (N>1), and the device field will
+	// be lost
+	filteredTags := make([]string, 0, len(serie.Tags))
+
+	for _, tag := range serie.Tags {
+		if strings.HasPrefix(tag, "device:") {
+			serie.Device = tag[7:]
+		} else {
+			filteredTags = append(filteredTags, tag)
+		}
+	}
+	serie.Tags = filteredTags
+}
+
+// MarshalJSON serializes timeseries to JSON so it can be sent to V1 endpoints
+//FIXME(maxime): to be removed when v2 endpoints are available
+func (series Series) MarshalJSON() ([]byte, error) {
+	// use an alias to avoid infinite recursion while serializing a Series
+	type SeriesAlias Series
+	for _, serie := range series {
+		populateDeviceField(serie)
+	}
+
+	data := map[string][]*Serie{
+		"series": SeriesAlias(series),
+	}
+	reqBody := &bytes.Buffer{}
+	err := json.NewEncoder(reqBody).Encode(data)
+	return reqBody.Bytes(), err
+}
+
+// SplitPayload breaks the payload into, at least, "times" number of pieces
+func (series Series) SplitPayload(times int) ([]marshaler.Marshaler, error) {
+	seriesExpvar.Add("TimesSplit", 1)
+
+	// We need to split series without splitting metrics across multiple
+	// payload. So we first group series by metric name.
+	metricsPerName := map[string]Series{}
+	for _, s := range series {
+		if _, ok := metricsPerName[s.Name]; ok {
+			metricsPerName[s.Name] = append(metricsPerName[s.Name], s)
+		} else {
+			metricsPerName[s.Name] = Series{s}
+		}
+	}
+
+	// if we only have one metric name we cannot split further
+	if len(metricsPerName) == 1 {
+		seriesExpvar.Add("SplitMetricsTooBig", 1)
+		return nil, fmt.Errorf("Cannot split metric '%s' into %d payload (it contains %d series)", series[0].Name, times, len(series))
+	}
+
+	nbSeriesPerPayload := len(series) / times
+
+	payloads := []marshaler.Marshaler{}
+	current := Series{}
+	for _, m := range metricsPerName {
+		// If on metric is bigger than the targeted size we directly
+		// add it as a payload.
+		if len(m) >= nbSeriesPerPayload {
+			payloads = append(payloads, m)
+			continue
+		}
+
+		// Then either append to the current payload if "m" is small
+		// enough or flush the current payload and start a new one.
+		// This may result in more than twice the number of payloads
+		// asked for but is "good enough" and will loop only once
+		// through metricsPerName
+		if len(current)+len(m) < nbSeriesPerPayload {
+			current = append(current, m...)
+		} else {
+			payloads = append(payloads, current)
+			current = m
+		}
+	}
+	if len(current) != 0 {
+		payloads = append(payloads, current)
+	}
+	return payloads, nil
+}
+
+// UnmarshalJSON is a custom unmarshaller for Point (used for testing)
+func (p *Point) UnmarshalJSON(buf []byte) error {
+	tmp := []interface{}{&p.Ts, &p.Value}
+	wantLen := len(tmp)
+	if err := json.Unmarshal(buf, &tmp); err != nil {
+		return err
+	}
+	if len(tmp) != wantLen {
+		return fmt.Errorf("wrong number of fields in Point: %d != %d", len(tmp), wantLen)
+	}
+	return nil
+}
+
+// String could be used for debug logging
+func (e Serie) String() string {
+	s, err := json.Marshal(e)
+	if err != nil {
+		return ""
+	}
+	return string(s)
+}
+
+//// The following methods implement the StreamJSONMarshaler interface
+//// for support of the enable_stream_payload_serialization option.
+
+// JSONHeader prints the payload header for this type
+func (series Series) JSONHeader() []byte {
+	return []byte(`{"series":[`)
+}
+
+// Len returns the number of items to marshal
+func (series Series) Len() int {
+	return len(series)
+}
+
+// JSONItem prints the json representation of an item
+func (series Series) JSONItem(i int) ([]byte, error) {
+	if i < 0 || i > len(series)-1 {
+		return nil, errors.New("out of range")
+	}
+	populateDeviceField(series[i])
+	return marshaller.Marshal(series[i])
+}
+
+// JSONFooter prints the payload footer for this type
+func (series Series) JSONFooter() []byte {
+	return []byte(`]}`)
+}
+
+// DescribeItem returns a text description for logs
+func (series Series) DescribeItem(i int) string {
+	if i < 0 || i > len(series)-1 {
+		return "out of range"
+	}
+	return fmt.Sprintf("name %q, %d points", series[i].Name, len(series[i].Points))
+}
+
+// encodePoints is registered to serialize a Point array with
+// limited reflections and heap allocations.
+// Called when using jsoniter
+func encodePoints(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+	if ptr == nil {
+		stream.Write(jsonArrayStart)
+		stream.Write(jsonArrayEnd)
+		return
+	}
+
+	points := *(*[]Point)(ptr)
+	var needComa bool
+	stream.Write(jsonArrayStart)
+	for _, p := range points {
+		if needComa {
+			stream.Write(jsonSeparator)
+		} else {
+			needComa = true
+		}
+		fmt.Fprintf(stream, "[%v,%v]", int64(p.Ts), p.Value)
+	}
+	stream.Write(jsonArrayEnd)
+}
+
+func init() {
+	jsoniter.RegisterTypeEncoderFunc(
+		"[]metrics.Point",
+		encodePoints,
+		func(ptr unsafe.Pointer) bool { return ptr == nil },
+	)
+}
+
+// GroupedSeries represents a list of Serie grouped by metric name
+type GroupedSeries [][]*Serie
+
+// SortAndGroupSeries groups a slice of series per metric name
+func SortAndGroupSeries(in Series) GroupedSeries {
+	if len(in) < 2 {
+		return GroupedSeries{in}
+	}
+
+	sort.Slice(in, func(i, j int) bool { return in[i].Name < in[j].Name })
+
+	var out GroupedSeries
+	groupName := in[0].Name
+	groupStart := 0
+
+	for i := 1; i < len(in); i++ {
+		if in[i].Name != groupName {
+			// Close previous slice
+			out = append(out, in[groupStart:i])
+
+			// Open new slice
+			groupName = in[i].Name
+			groupStart = i
+		}
+	}
+
+	// Close last slice
+	out = append(out, in[groupStart:])
+
+	return out
+}
+
+// JSONHeader prints the payload header for this type
+func (series GroupedSeries) JSONHeader() []byte {
+	return []byte(`{"series":[`)
+}
+
+// Len returns the number of items to marshal
+func (series GroupedSeries) Len() int {
+	return len(series)
+}
+
+// JSONItem prints the json representation of an item
+func (series GroupedSeries) JSONItem(i int) ([]byte, error) {
+	if i < 0 || i > len(series)-1 {
+		return nil, errors.New("out of range")
+	}
+
+	group := series[i]
+
+	var bufferSize int
+	if len(group) > 64 {
+		// Pre-allocate an appropriate buffer size
+		// assuming the item size will be similar
+		b, _ := marshaller.Marshal(group[0])
+		bufferSize = len(b) * len(group)
+	}
+
+	var needComa bool
+	jsonOut := jsoniter.NewStream(marshaller, nil, bufferSize)
+	for _, serie := range group {
+		if needComa {
+			jsonOut.Write(jsonSeparator)
+		} else {
+			needComa = true
+		}
+		populateDeviceField(serie)
+		jsonOut.WriteVal(serie)
+	}
+
+	return jsonOut.Buffer(), nil
+}
+
+// JSONFooter prints the payload footer for this type
+func (series GroupedSeries) JSONFooter() []byte {
+	return []byte(`]}`)
+}
+
+// DescribeItem returns a text description for logs
+func (series GroupedSeries) DescribeItem(i int) string {
+	if i < 0 || i > len(series)-1 {
+		return "out of range"
+	}
+	if len(series[i]) == 0 {
+		return "empty item"
+	}
+	return fmt.Sprintf("name %q, %d series", series[i][0].Name, len(series[i]))
+}
+
+// MarshalJSON is not implemented for GroupedSeries
+func (series GroupedSeries) MarshalJSON() ([]byte, error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+// Marshal is not implemented for GroupedSeries
+func (series GroupedSeries) Marshal() ([]byte, error) {
+	return nil, fmt.Errorf("not implemented")
+}
+
+// SplitPayload is not implemented for GroupedSeries
+func (series GroupedSeries) SplitPayload(int) ([]marshaler.Marshaler, error) {
+	return nil, fmt.Errorf("not implemented")
+}
---
+++ pkg/serializer/serializer_test.go
@@ -0,0 +1,346 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2018 Datadog, Inc.
+
+package serializer
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/DataDog/datadog-agent/pkg/config"
+	"github.com/DataDog/datadog-agent/pkg/forwarder"
+	"github.com/DataDog/datadog-agent/pkg/serializer/marshaler"
+	"github.com/DataDog/datadog-agent/pkg/util/compression"
+)
+
+var initialContentEncoding = compression.ContentEncoding
+
+func resetContentEncoding() {
+	compression.ContentEncoding = initialContentEncoding
+	initExtraHeaders()
+}
+
+func TestInitExtraHeadersNoopCompression(t *testing.T) {
+	compression.ContentEncoding = ""
+	defer resetContentEncoding()
+
+	initExtraHeaders()
+
+	expected := make(http.Header)
+	expected.Set("Content-Type", jsonContentType)
+	assert.Equal(t, expected, jsonExtraHeaders)
+
+	expected = make(http.Header)
+	expected.Set(payloadVersionHTTPHeader, AgentPayloadVersion)
+	expected.Set("Content-Type", protobufContentType)
+	assert.Equal(t, expected, protobufExtraHeaders)
+
+	// No "Content-Encoding" header
+	expected = make(http.Header)
+	expected.Set("Content-Type", jsonContentType)
+	assert.Equal(t, expected, jsonExtraHeadersWithCompression)
+
+	expected = make(http.Header)
+	expected.Set("Content-Type", protobufContentType)
+	expected.Set(payloadVersionHTTPHeader, AgentPayloadVersion)
+	assert.Equal(t, expected, protobufExtraHeadersWithCompression)
+}
+
+func TestInitExtraHeadersWithCompression(t *testing.T) {
+	compression.ContentEncoding = "zstd"
+	defer resetContentEncoding()
+
+	initExtraHeaders()
+
+	expected := make(http.Header)
+	expected.Set("Content-Type", jsonContentType)
+	assert.Equal(t, expected, jsonExtraHeaders)
+
+	expected = make(http.Header)
+	expected.Set("Content-Type", protobufContentType)
+	expected.Set(payloadVersionHTTPHeader, AgentPayloadVersion)
+	assert.Equal(t, expected, protobufExtraHeaders)
+
+	// "Content-Encoding" header present with correct value
+	expected = make(http.Header)
+	expected.Set("Content-Type", jsonContentType)
+	expected.Set("Content-Encoding", compression.ContentEncoding)
+	assert.Equal(t, expected, jsonExtraHeadersWithCompression)
+
+	expected = make(http.Header)
+	expected.Set("Content-Type", protobufContentType)
+	expected.Set("Content-Encoding", compression.ContentEncoding)
+	expected.Set(payloadVersionHTTPHeader, AgentPayloadVersion)
+	assert.Equal(t, expected, protobufExtraHeadersWithCompression)
+}
+
+func TestAgentPayloadVersion(t *testing.T) {
+	assert.NotEmpty(t, AgentPayloadVersion, "AgentPayloadVersion is empty, indicates that the package was not built correctly")
+}
+
+var (
+	jsonPayloads     = forwarder.Payloads{}
+	protobufPayloads = forwarder.Payloads{}
+	jsonHeader       = []byte("{")
+	jsonFooter       = []byte("}")
+	jsonItem         = []byte("TO JSON")
+	jsonString       = []byte("{TO JSON}")
+	protobufString   = []byte("TO PROTOBUF")
+)
+
+func init() {
+	jsonPayloads, _ = mkPayloads(jsonString, true)
+	protobufPayloads, _ = mkPayloads(protobufString, true)
+}
+
+type testPayload struct{}
+
+func (p *testPayload) MarshalJSON() ([]byte, error) { return jsonString, nil }
+func (p *testPayload) Marshal() ([]byte, error)     { return protobufString, nil }
+func (p *testPayload) SplitPayload(int) ([]marshaler.Marshaler, error) {
+	return []marshaler.Marshaler{}, nil
+}
+func (p *testPayload) JSONHeader() []byte             { return jsonHeader }
+func (p *testPayload) Len() int                       { return 1 }
+func (p *testPayload) JSONItem(i int) ([]byte, error) { return jsonItem, nil }
+func (p *testPayload) DescribeItem(i int) string      { return "description" }
+func (p *testPayload) JSONFooter() []byte             { return jsonFooter }
+
+type testErrorPayload struct{}
+
+func (p *testErrorPayload) MarshalJSON() ([]byte, error) { return nil, fmt.Errorf("some error") }
+func (p *testErrorPayload) Marshal() ([]byte, error)     { return nil, fmt.Errorf("some error") }
+func (p *testErrorPayload) SplitPayload(int) ([]marshaler.Marshaler, error) {
+	return []marshaler.Marshaler{}, fmt.Errorf("some error")
+}
+func (p *testErrorPayload) JSONHeader() []byte             { return jsonHeader }
+func (p *testErrorPayload) Len() int                       { return 1 }
+func (p *testErrorPayload) JSONItem(i int) ([]byte, error) { return jsonItem, fmt.Errorf("some error") }
+func (p *testErrorPayload) DescribeItem(i int) string      { return "description" }
+func (p *testErrorPayload) JSONFooter() []byte             { return jsonFooter }
+
+func mkPayloads(payload []byte, compress bool) (forwarder.Payloads, error) {
+	payloads := forwarder.Payloads{}
+	var err error
+	if compress {
+		payload, err = compression.Compress(nil, payload)
+		if err != nil {
+			return nil, err
+		}
+	}
+	payloads = append(payloads, &payload)
+	return payloads, nil
+}
+
+func TestSendV1Events(t *testing.T) {
+	f := &forwarder.MockedForwarder{}
+	f.On("SubmitV1Intake", jsonPayloads, jsonExtraHeadersWithCompression).Return(nil).Times(1)
+
+	s := NewSerializer(f)
+
+	payload := &testPayload{}
+	err := s.SendEvents(payload)
+	require.Nil(t, err)
+	f.AssertExpectations(t)
+
+	errPayload := &testErrorPayload{}
+	err = s.SendEvents(errPayload)
+	require.NotNil(t, err)
+}
+
+func TestSendEvents(t *testing.T) {
+	f := &forwarder.MockedForwarder{}
+	f.On("SubmitEvents", protobufPayloads, protobufExtraHeadersWithCompression).Return(nil).Times(1)
+	config.Datadog.Set("use_v2_api.events", true)
+	defer config.Datadog.Set("use_v2_api.events", nil)
+
+	s := NewSerializer(f)
+
+	payload := &testPayload{}
+	err := s.SendEvents(payload)
+	require.Nil(t, err)
+	f.AssertExpectations(t)
+
+	errPayload := &testErrorPayload{}
+	err = s.SendEvents(errPayload)
+	require.NotNil(t, err)
+}
+
+func TestSendV1ServiceChecks(t *testing.T) {
+	f := &forwarder.MockedForwarder{}
+	f.On("SubmitV1CheckRuns", jsonPayloads, jsonExtraHeadersWithCompression).Return(nil).Times(1)
+
+	s := NewSerializer(f)
+
+	payload := &testPayload{}
+	err := s.SendServiceChecks(payload)
+	require.Nil(t, err)
+	f.AssertExpectations(t)
+
+	errPayload := &testErrorPayload{}
+	err = s.SendServiceChecks(errPayload)
+	require.NotNil(t, err)
+}
+
+func TestSendServiceChecks(t *testing.T) {
+	f := &forwarder.MockedForwarder{}
+	f.On("SubmitServiceChecks", protobufPayloads, protobufExtraHeadersWithCompression).Return(nil).Times(1)
+	config.Datadog.Set("use_v2_api.service_checks", true)
+	defer config.Datadog.Set("use_v2_api.service_checks", nil)
+
+	s := NewSerializer(f)
+
+	payload := &testPayload{}
+	err := s.SendServiceChecks(payload)
+	require.Nil(t, err)
+	f.AssertExpectations(t)
+
+	errPayload := &testErrorPayload{}
+	err = s.SendServiceChecks(errPayload)
+	require.NotNil(t, err)
+}
+
+func TestSendV1Series(t *testing.T) {
+	f := &forwarder.MockedForwarder{}
+	f.On("SubmitV1Series", jsonPayloads, jsonExtraHeadersWithCompression).Return(nil).Times(1)
+	config.Datadog.Set("enable_stream_payload_serialization", false)
+	defer config.Datadog.Set("enable_stream_payload_serialization", nil)
+
+	s := NewSerializer(f)
+
+	payload := &testPayload{}
+	err := s.SendSeries(payload)
+	require.Nil(t, err)
+	f.AssertExpectations(t)
+
+	errPayload := &testErrorPayload{}
+	err = s.SendSeries(errPayload)
+	require.NotNil(t, err)
+}
+
+func TestSendSeries(t *testing.T) {
+	f := &forwarder.MockedForwarder{}
+	f.On("SubmitSeries", protobufPayloads, protobufExtraHeadersWithCompression).Return(nil).Times(1)
+	config.Datadog.Set("use_v2_api.series", true)
+	defer config.Datadog.Set("use_v2_api.series", nil)
+
+	s := NewSerializer(f)
+
+	payload := &testPayload{}
+	err := s.SendSeries(payload)
+	require.Nil(t, err)
+	f.AssertExpectations(t)
+
+	errPayload := &testErrorPayload{}
+	err = s.SendSeries(errPayload)
+	require.NotNil(t, err)
+}
+
+func TestSendSketch(t *testing.T) {
+	f := &forwarder.MockedForwarder{}
+	payloads, _ := mkPayloads(protobufString, false)
+	f.On("SubmitSketchSeries", payloads, protobufExtraHeaders).Return(nil).Times(1)
+
+	s := NewSerializer(f)
+
+	payload := &testPayload{}
+	err := s.SendSketch(payload)
+	require.Nil(t, err)
+	f.AssertExpectations(t)
+
+	errPayload := &testErrorPayload{}
+	err = s.SendSketch(errPayload)
+	require.NotNil(t, err)
+}
+
+func TestSendMetadata(t *testing.T) {
+	f := &forwarder.MockedForwarder{}
+	payloads, _ := mkPayloads(jsonString, false)
+	f.On("SubmitV1Intake", payloads, jsonExtraHeaders).Return(nil).Times(1)
+
+	s := NewSerializer(f)
+
+	payload := &testPayload{}
+	err := s.SendMetadata(payload)
+	require.Nil(t, err)
+	f.AssertExpectations(t)
+
+	f.On("SubmitV1Intake", payloads, jsonExtraHeaders).Return(fmt.Errorf("some error")).Times(1)
+	err = s.SendMetadata(payload)
+	require.NotNil(t, err)
+	f.AssertExpectations(t)
+
+	errPayload := &testErrorPayload{}
+	err = s.SendMetadata(errPayload)
+	require.NotNil(t, err)
+}
+
+func TestSendJSONToV1Intake(t *testing.T) {
+	f := &forwarder.MockedForwarder{}
+	payload := []byte("\"test\"")
+	payloads, _ := mkPayloads(payload, false)
+	f.On("SubmitV1Intake", payloads, jsonExtraHeaders).Return(nil).Times(1)
+
+	s := NewSerializer(f)
+
+	err := s.SendJSONToV1Intake("test")
+	require.Nil(t, err)
+	f.AssertExpectations(t)
+
+	f.On("SubmitV1Intake", payloads, jsonExtraHeaders).Return(fmt.Errorf("some error")).Times(1)
+	err = s.SendJSONToV1Intake("test")
+	require.NotNil(t, err)
+	f.AssertExpectations(t)
+
+	errPayload := &testErrorPayload{}
+	err = s.SendJSONToV1Intake(errPayload)
+	require.NotNil(t, err)
+}
+
+func TestSendWithDisabledKind(t *testing.T) {
+	config.Datadog.Set("enable_payloads.events", false)
+	config.Datadog.Set("enable_payloads.series", false)
+	config.Datadog.Set("enable_payloads.service_checks", false)
+	config.Datadog.Set("enable_payloads.sketches", false)
+	config.Datadog.Set("enable_payloads.json_to_v1_intake", false)
+
+	//restore default values
+	defer func() {
+		config.Datadog.Set("enable_payloads.events", true)
+		config.Datadog.Set("enable_payloads.series", true)
+		config.Datadog.Set("enable_payloads.service_checks", true)
+		config.Datadog.Set("enable_payloads.sketches", true)
+		config.Datadog.Set("enable_payloads.json_to_v1_intake", true)
+	}()
+
+	f := &forwarder.MockedForwarder{}
+	s := NewSerializer(f)
+
+	payload := &testPayload{}
+	payloads, _ := mkPayloads(jsonString, false)
+
+	s.SendEvents(payload)
+	s.SendSeries(payload)
+	s.SendSketch(payload)
+	s.SendServiceChecks(payload)
+	s.SendJSONToV1Intake("test")
+
+	f.AssertNotCalled(t, "SubmitV1Intake")
+	f.AssertNotCalled(t, "SubmitEvents")
+	f.AssertNotCalled(t, "SubmitV1CheckRuns")
+	f.AssertNotCalled(t, "SubmitServiceChecks")
+	f.AssertNotCalled(t, "SubmitV1Series")
+	f.AssertNotCalled(t, "SubmitSeries")
+	f.AssertNotCalled(t, "SubmitSketchSeries")
+
+	// We never disable metadata
+	f.On("SubmitV1Intake", payloads, jsonExtraHeaders).Return(nil).Times(1)
+	s.SendMetadata(payload)
+	f.AssertNumberOfCalls(t, "SubmitV1Intake", 1) // called once for the metadata
+}
