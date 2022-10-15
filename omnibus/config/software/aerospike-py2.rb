cae81a2ce5b94e3d5c96a5bac56325e0b2bec916
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:29:31 -0400

    Initialize kserrania/solve-linker-issues-aerospike

---
+++ omnibus/config/software/aerospike-py2.rb
@@ -0,0 +1,17 @@
+name "aerospike-py2"
+default_version "3.10.0"
+
+dependency "aerospike-client"
+dependency "pip2"
+
+build do
+  env = {
+    "DOWNLOAD_C_CLIENT" => "0",
+    "AEROSPIKE_C_HOME" => "#{install_dir}/embedded/lib/aerospike",
+    "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+    "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
+    "LD_RUN_PATH" => "#{install_dir}/embedded/lib",
+  }
+
+  command "#{install_dir}/embedded/bin/pip2 install --no-binary :all: aerospike==#{version}", :env => env
+end
