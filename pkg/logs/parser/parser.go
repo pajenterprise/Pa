edf3d4b449ec4f5556ad150c40a7a15e1d704dbe
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:13:06 -0400

    Initialize lee.avital/inv_fix

---
+++ pkg/logs/parser/parser.go
@@ -0,0 +1,23 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2019 Datadog, Inc.
+
+package parser
+
+// NoopParser is the default parser and does nothing
+var NoopParser *noopParser
+
+// Parser parse messages
+type Parser interface {
+	Parse([]byte) ([]byte, string, string, error)
+}
+
+type noopParser struct {
+	Parser
+}
+
+// Parse does nothing for NoopParser
+func (p *noopParser) Parse(msg []byte) ([]byte, string, string, error) {
+	return msg, "", "", nil
+}
