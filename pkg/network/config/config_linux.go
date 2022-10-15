07f2f8d7e851bcb2ab4ec0e39dc99c6b7ba2c125
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 22:28:59 -0400

    Initialize jaime/debugnopy

---
+++ pkg/network/config/config_linux.go
@@ -0,0 +1,55 @@
+// +build linux_bpf
+
+package config
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/network/ebpf/probes"
+)
+
+// EnabledProbes returns a map of probes that are enabled per config settings.
+// This map does not include the probes used exclusively in the offset guessing process.
+func (c *Config) EnabledProbes(pre410Kernel bool) (map[probes.ProbeName]struct{}, error) {
+	enabled := make(map[probes.ProbeName]struct{}, 0)
+
+	if c.CollectTCPConns {
+		if pre410Kernel {
+			enabled[probes.TCPSendMsgPre410] = struct{}{}
+		} else {
+			enabled[probes.TCPSendMsg] = struct{}{}
+		}
+		enabled[probes.TCPCleanupRBuf] = struct{}{}
+		enabled[probes.TCPClose] = struct{}{}
+		enabled[probes.TCPCloseReturn] = struct{}{}
+		enabled[probes.TCPRetransmit] = struct{}{}
+		enabled[probes.InetCskAcceptReturn] = struct{}{}
+		enabled[probes.TCPv4DestroySock] = struct{}{}
+		enabled[probes.TCPSetState] = struct{}{}
+
+		if c.BPFDebug || c.EnableHTTPMonitoring {
+			enabled[probes.TCPSendMsgReturn] = struct{}{}
+		}
+	}
+
+	if c.CollectUDPConns {
+		enabled[probes.UDPRecvMsgReturn] = struct{}{}
+		enabled[probes.UDPDestroySock] = struct{}{}
+		enabled[probes.UDPDestroySockReturn] = struct{}{}
+		enabled[probes.IPMakeSkb] = struct{}{}
+		enabled[probes.InetBind] = struct{}{}
+		enabled[probes.InetBindRet] = struct{}{}
+
+		if c.CollectIPv6Conns {
+			enabled[probes.IP6MakeSkb] = struct{}{}
+			enabled[probes.Inet6Bind] = struct{}{}
+			enabled[probes.Inet6BindRet] = struct{}{}
+		}
+
+		if pre410Kernel {
+			enabled[probes.UDPRecvMsgPre410] = struct{}{}
+		} else {
+			enabled[probes.UDPRecvMsg] = struct{}{}
+		}
+	}
+
+	return enabled, nil
+}
