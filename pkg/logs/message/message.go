7f1b6a34f8b8bf7f066f8e1e003d3fde93bc2cbc
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:05:04 -0400

    Initialize dca-token-secret

---
+++ pkg/logs/message/message.go
@@ -0,0 +1,52 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2018 Datadog, Inc.
+
+package message
+
+// Message represents a log line sent to datadog, with its metadata
+type Message interface {
+	Content() []byte
+	SetContent([]byte)
+	GetOrigin() *Origin
+	GetStatus() string
+}
+
+type message struct {
+	content []byte
+	origin  *Origin
+	status  string
+}
+
+// New returns a new Message
+func New(content []byte, origin *Origin, status string) Message {
+	return &message{
+		content: content,
+		origin:  origin,
+		status:  status,
+	}
+}
+
+// Content returns the content the message, the actual log line
+func (m *message) Content() []byte {
+	return m.content
+}
+
+// SetContent updates the content the message
+func (m *message) SetContent(content []byte) {
+	m.content = content
+}
+
+// GetOrigin returns the Origin from which the message comes
+func (m *message) GetOrigin() *Origin {
+	return m.origin
+}
+
+// GetStatus returns the status of the message
+func (m *message) GetStatus() string {
+	if m.status != "" {
+		return m.status
+	}
+	return StatusInfo
+}
---
+++ tasks/go.py
@@ -0,0 +1,204 @@
+"""
+Golang related tasks go here
+"""
+from __future__ import print_function
+import os
+
+from invoke import task
+from invoke.exceptions import Exit
+from .build_tags import get_default_build_tags
+
+
+# List of modules to ignore when running lint on Windows platform
+WIN_MODULE_WHITELIST = [
+    "iostats_wmi_windows.go",
+    "iostats_pdh_windows.go",
+    "pdh.go",
+    "pdhhelper.go",
+    "doflare.go",
+]
+
+# List of paths to ignore in misspell's output
+MISSPELL_IGNORED_TARGETS = [
+    os.path.join("cmd", "agent", "dist", "checks", "prometheus_check"),
+    os.path.join("cmd", "agent", "gui", "views", "private"),
+]
+
+
+@task
+def fmt(ctx, targets, fail_on_fmt=False):
+    """
+    Run go fmt on targets.
+
+    Example invokation:
+        inv fmt --targets=./pkg/collector/check,./pkg/aggregator
+    """
+    if isinstance(targets, basestring):
+        # when this function is called from the command line, targets are passed
+        # as comma separated tokens in a string
+        targets = targets.split(',')
+
+    result = ctx.run("gofmt -l -w -s " + " ".join(targets))
+    if result.stdout:
+        files = {x for x in result.stdout.split("\n") if x}
+        print("Reformatted the following files: {}".format(','.join(files)))
+        if fail_on_fmt:
+            print("Code was not properly formatted, exiting...")
+            raise Exit(code=1)
+    print("gofmt found no issues")
+
+
+@task
+def lint(ctx, targets):
+    """
+    Run golint on targets. If targets are not specified,
+    the value from `invoke.yaml` will be used.
+
+    Example invokation:
+        inv lint --targets=./pkg/collector/check,./pkg/aggregator
+    """
+    if isinstance(targets, basestring):
+        # when this function is called from the command line, targets are passed
+        # as comma separated tokens in a string
+        targets = targets.split(',')
+
+    # add the /... suffix to the targets
+    targets_list = ["{}/...".format(t) for t in targets]
+    result = ctx.run("golint {}".format(' '.join(targets_list)))
+    if result.stdout:
+        files = []
+        skipped_files = set()
+        for line in (out for out in result.stdout.split('\n') if out):
+            fname = os.path.basename(line.split(":")[0])
+            if fname in WIN_MODULE_WHITELIST:
+                skipped_files.add(fname)
+                continue
+            files.append(fname)
+
+        if files:
+            print("Linting issues found in {} files.".format(len(files)))
+            raise Exit(code=1)
+
+        if skipped_files:
+            for skipped in skipped_files:
+                print("Allowed errors in whitelisted file {}".format(skipped))
+
+    print("golint found no issues")
+
+
+@task
+def vet(ctx, targets):
+    """
+    Run go vet on targets.
+
+    Example invokation:
+        inv vet --targets=./pkg/collector/check,./pkg/aggregator
+    """
+    if isinstance(targets, basestring):
+        # when this function is called from the command line, targets are passed
+        # as comma separated tokens in a string
+        targets = targets.split(',')
+
+    # add the /... suffix to the targets
+    args = ["{}/...".format(t) for t in targets]
+    build_tags = get_default_build_tags()
+    ctx.run("go vet -tags \"{}\" ".format(" ".join(build_tags)) + " ".join(args))
+    # go vet exits with status 1 when it finds an issue, if we're here
+    # everything went smooth
+    print("go vet found no issues")
+
+
+@task
+def cyclo(ctx, targets, limit=15):
+    """
+    Run gocyclo on targets.
+    Use the 'limit' parameter to change the maximum cyclic complexity.
+
+    Example invokation:
+        inv cyclo --targets=./pkg/collector/check,./pkg/aggregator
+    """
+    if isinstance(targets, basestring):
+        # when this function is called from the command line, targets are passed
+        # as comma separated tokens in a string
+        targets = targets.split(',')
+
+    ctx.run("gocyclo -over {} ".format(limit) + " ".join(targets))
+    # gocyclo exits with status 1 when it finds an issue, if we're here
+    # everything went smooth
+    print("gocyclo found no issues")
+
+
+@task
+def ineffassign(ctx, targets):
+    """
+    Run ineffassign on targets.
+
+    Example invokation:
+        inv ineffassign --targets=./pkg/collector/check,./pkg/aggregator
+    """
+    if isinstance(targets, basestring):
+        # when this function is called from the command line, targets are passed
+        # as comma separated tokens in a string
+        targets = targets.split(',')
+
+    ctx.run("ineffassign " + " ".join(targets))
+    # ineffassign exits with status 1 when it finds an issue, if we're here
+    # everything went smooth
+    print("ineffassign found no issues")
+
+
+@task
+def misspell(ctx, targets):
+    """
+    Run misspell on targets.
+
+    Example invokation:
+        inv misspell --targets=./pkg/collector/check,./pkg/aggregator
+    """
+    if isinstance(targets, basestring):
+        # when this function is called from the command line, targets are passed
+        # as comma separated tokens in a string
+        targets = targets.split(',')
+
+    result = ctx.run("misspell " + " ".join(targets), hide=True)
+    legit_misspells = []
+    for found_misspell in result.stdout.split("\n"):
+        if len(found_misspell.strip()) > 0:
+            if not any([ignored_target in found_misspell for ignored_target in MISSPELL_IGNORED_TARGETS]):
+                legit_misspells.append(found_misspell)
+
+    if len(legit_misspells) > 0:
+        print("Misspell issues found:\n" + "\n".join(legit_misspells))
+        raise Exit(code=2)
+    else:
+        print("misspell found no issues")
+
+@task
+def deps(ctx):
+    """
+    Setup Go dependencies
+    """
+    ctx.run("go get -u github.com/golang/dep/cmd/dep")
+    ctx.run("go get -u github.com/golang/lint/golint")
+    ctx.run("go get -u github.com/fzipp/gocyclo")
+    ctx.run("go get -u github.com/gordonklaus/ineffassign")
+    ctx.run("go get -u github.com/client9/misspell/cmd/misspell")
+    ctx.run("dep ensure")
+
+
+@task
+def reset(ctx):
+    """
+    Clean everything and remove vendoring
+    """
+    # go clean
+    print("Executing go clean")
+    ctx.run("go clean")
+
+    # remove the bin/ folder
+    print("Remove agent binary folder")
+    ctx.run("rm -rf ./bin/")
+
+    # remove vendor folder
+    print("Remove vendor folder")
+    ctx.run("rm -rf ./vendor")
