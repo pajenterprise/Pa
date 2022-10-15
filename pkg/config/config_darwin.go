95a0a93759450b7f6362ee2004cab171a11f9159
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:09:39 -0400

    Initialize db/fixpath

---
+++ pkg/config/config_darwin.go
@@ -0,0 +1,22 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2018 Datadog, Inc.
+
+package config
+
+const (
+	defaultConfdPath            = "/opt/datadog-agent/etc/conf.d"
+	defaultAdditionalChecksPath = "/opt/datadog-agent/etc/checks.d"
+	defaultRunPath              = "/opt/datadog-agent/run"
+	defaultSyslogURI            = "unixgram:///var/run/syslog"
+	defaultGuiPort              = "5002"
+)
+
+// called by init in config.go, to ensure any os-specific config is done
+// in time
+func osinit() {
+}
+
+// NewAssetFs  Should never be called on non-android
+func setAssetFs(config Config) {}
