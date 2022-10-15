b1327879df3cd30bd0ab08ec500cc710d4fb18f4
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:17:54 -0400

    Initialize johanmorganti-add-runtime-metric-standardstatsd

---
+++ pkg/collector/corechecks/embed/jmx/runner.go
@@ -0,0 +1,58 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2019 Datadog, Inc.
+
+// +build jmx
+
+package jmx
+
+import (
+	"runtime"
+
+	"github.com/DataDog/datadog-agent/pkg/autodiscovery/integration"
+	"github.com/DataDog/datadog-agent/pkg/config"
+	"github.com/DataDog/datadog-agent/pkg/jmxfetch"
+)
+
+type runner struct {
+	jmxfetch *jmxfetch.JMXFetch
+	started  bool
+}
+
+func (r *runner) initRunner() {
+	r.jmxfetch = &jmxfetch.JMXFetch{}
+	r.jmxfetch.LogLevel = config.Datadog.GetString("log_level")
+}
+
+func (r *runner) startRunner() error {
+
+	lifecycleMgmt := true
+	if runtime.GOOS == "windows" {
+		lifecycleMgmt = false
+	}
+
+	err := r.jmxfetch.Start(lifecycleMgmt)
+	if err != nil {
+		return err
+	}
+	r.started = true
+	return nil
+}
+
+func (r *runner) configureRunner(instance, initConfig integration.Data) error {
+	if err := r.jmxfetch.ConfigureFromInstance(instance); err != nil {
+		return err
+	}
+	if err := r.jmxfetch.ConfigureFromInitConfig(initConfig); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *runner) stopRunner() error {
+	if r.jmxfetch != nil && r.started {
+		return r.jmxfetch.Stop()
+	}
+	return nil
+}
