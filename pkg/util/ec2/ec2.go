1841a256836ba75007d34b48417d7cc70ef6827c
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:10:35 -0400

    Initialize aks-hostname-mitigation

---
+++ pkg/util/ec2/ec2.go
@@ -0,0 +1,98 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2019 Datadog, Inc.
+
+package ec2
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+)
+
+// declare these as vars not const to ease testing
+var (
+	metadataURL         = "http://169.254.169.254/latest/meta-data"
+	instanceIdentityURL = "http://169.254.169.254/latest/dynamic/instance-identity/document/"
+	timeout             = 100 * time.Millisecond
+	defaultPrefixes     = []string{"ip-", "domu"}
+)
+
+// GetInstanceID fetches the instance id for current host from the EC2 metadata API
+func GetInstanceID() (string, error) {
+	return getMetadataItemWithMaxLength("/instance-id", 255)
+}
+
+// GetHostname fetches the hostname for current host from the EC2 metadata API
+func GetHostname() (string, error) {
+	return getMetadataItemWithMaxLength("/hostname", 255)
+}
+
+func getMetadataItemWithMaxLength(endpoint string, maxLength int) (string, error) {
+	metadataItem, err := getMetadataItem(endpoint)
+	if err != nil {
+		return "", err
+	}
+	if len(metadataItem) > maxLength {
+		return "", fmt.Errorf("ec2 metadata item %v is too long (%v)", endpoint, maxLength)
+	}
+	return metadataItem, nil
+}
+
+func getMetadataItem(endpoint string) (string, error) {
+	res, err := getResponse(metadataURL + endpoint)
+	if err != nil {
+		return "", fmt.Errorf("unable to fetch EC2 API, %s", err)
+	}
+
+	defer res.Body.Close()
+	all, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", fmt.Errorf("unable to read response body, %s", err)
+	}
+
+	return string(all), nil
+}
+
+func getResponse(url string) (*http.Response, error) {
+	client := http.Client{
+		Timeout: timeout,
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("status code %d trying to fetch %s", res.StatusCode, url)
+	}
+
+	return res, nil
+}
+
+// IsDefaultHostname returns whether the given hostname is a default one for EC2
+func IsDefaultHostname(hostname string) bool {
+	hostname = strings.ToLower(hostname)
+	isDefault := false
+	for _, val := range defaultPrefixes {
+		isDefault = isDefault || strings.HasPrefix(hostname, val)
+	}
+	return isDefault
+}
+
+// HostnameProvider gets the hostname
+func HostnameProvider() (string, error) {
+	log.Debug("GetHostname trying EC2 metadata...")
+	return GetInstanceID()
+}
