f2fec09f249de8aa9092b6f50e6a69a0f8f67932
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:05:41 -0400

    Initialize jaime/snmpwork

---
+++ pkg/logs/input/listener/tcp.go
@@ -0,0 +1,85 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2018 Datadog, Inc.
+
+package listener
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+
+	"github.com/DataDog/datadog-agent/pkg/logs/config"
+	"github.com/DataDog/datadog-agent/pkg/logs/pipeline"
+)
+
+// A TCPListener listens and accepts TCP connections and delegates the work to connHandler
+type TCPListener struct {
+	port        int
+	listener    net.Listener
+	connHandler *ConnectionHandler
+	stop        chan struct{}
+	done        chan struct{}
+}
+
+// NewTCPListener returns an initialized TCPListener
+func NewTCPListener(pp pipeline.Provider, source *config.LogSource) (*TCPListener, error) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", source.Config.Port))
+	if err != nil {
+		source.Status.Error(err)
+		return nil, err
+	}
+	source.Status.Success()
+	connHandler := NewConnectionHandler(pp, source)
+	return &TCPListener{
+		port:        source.Config.Port,
+		listener:    listener,
+		connHandler: connHandler,
+		stop:        make(chan struct{}, 1),
+		done:        make(chan struct{}, 1),
+	}, nil
+}
+
+// Start listens to TCP connections on another routine
+func (l *TCPListener) Start() {
+	log.Info("Starting TCP forwarder on port ", l.port)
+	l.connHandler.Start()
+	go l.run()
+}
+
+// Stop prevents the listener to accept new incoming connections
+// it blocks until connHandler is flushed
+func (l *TCPListener) Stop() {
+	log.Info("Stopping TCP forwarder on port ", l.port)
+	l.stop <- struct{}{}
+	l.listener.Close()
+	<-l.done
+}
+
+// run accepts new TCP connections and lets connHandler handle them
+func (l *TCPListener) run() {
+	defer func() {
+		l.listener.Close()
+		l.connHandler.Stop()
+		l.done <- struct{}{}
+	}()
+	for {
+		select {
+		case <-l.stop:
+			// stop accepting new connections
+			return
+		default:
+			conn, err := l.listener.Accept()
+			if err != nil {
+				// an error occurred, stop from accepting new connections
+				l.connHandler.source.Status.Error(err)
+				log.Error("Can't listen: ", err)
+				return
+			}
+			l.connHandler.source.Status.Success()
+			go l.connHandler.HandleConnection(conn)
+		}
+	}
+}
