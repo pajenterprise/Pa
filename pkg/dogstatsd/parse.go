823ae85ddae7f127b9489e49d572cfbeebd8abad
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:22:43 -0400

    Initialize arbll/dsd-intern-strings-poc

---
+++ pkg/dogstatsd/parse.go
@@ -0,0 +1,84 @@
+package dogstatsd
+
+import (
+	"bytes"
+	"strconv"
+	"unsafe"
+)
+
+type messageType int
+
+const (
+	metricSampleType messageType = iota
+	serviceCheckType
+	eventType
+)
+
+var (
+	eventPrefix        = []byte("_e{")
+	serviceCheckPrefix = []byte("_sc")
+
+	fieldSeparator = []byte("|")
+	colonSeparator = []byte(":")
+	commaSeparator = []byte(",")
+
+	commaSeparatorString = ","
+)
+
+func findMessageType(message []byte) messageType {
+	if bytes.HasPrefix(message, eventPrefix) {
+		return eventType
+	} else if bytes.HasPrefix(message, serviceCheckPrefix) {
+		return serviceCheckType
+	}
+	// Note that random gibberish is interpreted as a metric since they don't
+	// contain any easily identifiable feature
+	return metricSampleType
+}
+
+// nextField returns the data found before the first fieldSeparator and
+// the remainder, as a no-heap alternative to bytes.Split.
+// If the separator is not found, the remainder is nil.
+func nextField(message []byte) ([]byte, []byte) {
+	sepIndex := bytes.Index(message, fieldSeparator)
+	if sepIndex == -1 {
+		return message, nil
+	}
+	return message[:sepIndex], message[sepIndex+1:]
+}
+
+func parseTags(rawTags []byte, interner *stringInterner) []string {
+	if len(rawTags) == 0 {
+		return nil
+	}
+	tagsCount := bytes.Count(rawTags, commaSeparator) + 1
+	tags := make([]string, tagsCount)
+	i := 0
+	for i < (tagsCount - 1) {
+		tagIndex := bytes.Index(rawTags, commaSeparator)
+		if tagIndex < 0 {
+			break
+		}
+		tags[i] = interner.LoadOrStore(rawTags[:tagIndex])
+		rawTags = rawTags[tagIndex+len(commaSeparator):]
+		i++
+	}
+	tags[i] = interner.LoadOrStore(rawTags)
+	return tags[:i+1]
+}
+
+// the std API does not have methods to do []byte => float parsing
+// we use this unsafe trick to avoid having to allocate one string for
+// every parsed float
+// see https://github.com/golang/go/issues/2632
+func parseFloat64(rawFloat []byte) (float64, error) {
+	return strconv.ParseFloat(*(*string)(unsafe.Pointer(&rawFloat)), 64)
+}
+
+// the std API does not have methods to do []byte => float parsing
+// we use this unsafe trick to avoid having to allocate one string for
+// every parsed float
+// see https://github.com/golang/go/issues/2632
+func parseInt64(rawInt []byte) (int64, error) {
+	return strconv.ParseInt(*(*string)(unsafe.Pointer(&rawInt)), 10, 64)
+}
---
+++ pkg/metrics/test_helper.go
@@ -0,0 +1,118 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2019 Datadog, Inc.
+
+package metrics
+
+import (
+	"sort"
+
+	// stdlib
+	"fmt"
+	"testing"
+
+	// 3p
+
+	"github.com/DataDog/datadog-agent/pkg/aggregator/ckey"
+	"github.com/stretchr/testify/assert"
+)
+
+// AssertPointsEqual evaluate if two list of point are equal (order doesn't matters).
+func AssertPointsEqual(t *testing.T, expected, actual []Point) {
+	if assert.Equal(t, len(expected), len(actual)) {
+		for _, point := range expected {
+			assert.Contains(t, actual, point)
+		}
+	}
+}
+
+// AssertTagsEqual evaluate if two list of tags are equal (the order doesn't matters).
+func AssertTagsEqual(t assert.TestingT, expected, actual []string) {
+	if assert.Equal(t, len(expected), len(actual), fmt.Sprintf("Unexpected number of tags: expected %s, actual: %s", expected, actual)) {
+		for _, tag := range expected {
+			assert.Contains(t, actual, tag)
+		}
+	}
+}
+
+// AssertSeriesEqual evaluate if two list of series match
+func AssertSeriesEqual(t *testing.T, expected Series, series Series) {
+	assert.Equal(t, len(expected), len(series))
+	for _, serie := range series {
+		found := false
+		for _, expectedSerie := range expected {
+			if ckey.Compare(serie.ContextKey, expectedSerie.ContextKey) == 0 {
+				AssertSerieEqual(t, expectedSerie, serie)
+				found = true
+			}
+		}
+		assert.True(t, found)
+	}
+}
+
+// AssertSerieEqual evaluate if two are equal.
+func AssertSerieEqual(t *testing.T, expected, actual *Serie) {
+	assert.Equal(t, expected.Name, actual.Name)
+	if expected.Tags != nil {
+		assert.NotNil(t, actual.Tags)
+		AssertTagsEqual(t, expected.Tags, actual.Tags)
+	}
+	assert.Equal(t, expected.Host, actual.Host)
+	assert.Equal(t, expected.MType, actual.MType)
+	assert.Equal(t, expected.Interval, actual.Interval)
+	assert.Equal(t, expected.SourceTypeName, actual.SourceTypeName)
+	if !expected.ContextKey.IsZero() {
+		// Only test the contextKey if it's set in the expected Serie
+		assert.Equal(t, expected.ContextKey, actual.ContextKey)
+	}
+	assert.Equal(t, expected.NameSuffix, actual.NameSuffix)
+	AssertPointsEqual(t, expected.Points, actual.Points)
+}
+
+// AssertSketchSeriesEqual checks whether two SketchSeries are equal
+func AssertSketchSeriesEqual(t assert.TestingT, exp, act SketchSeries) {
+	if h, ok := t.(tHelper); ok {
+		h.Helper()
+	}
+	assert.Equal(t, exp.Name, act.Name, "Name")
+
+	switch {
+	case len(exp.Tags) == 0:
+		assert.Len(t, act.Tags, 0, "(act) Tags: should be empty")
+	case len(act.Tags) == 0:
+		assert.Len(t, exp.Tags, 0, "(act) Tags: shouldn't be empty")
+	default:
+		AssertTagsEqual(t, exp.Tags, act.Tags)
+	}
+
+	assert.Equal(t, exp.Host, act.Host, "Host")
+	assert.Equal(t, exp.Interval, act.Interval, "Interval")
+	assert.Equal(t, exp.ContextKey, act.ContextKey, "ContextKey")
+
+	switch {
+	case len(exp.Points) != len(act.Points):
+		t.Errorf("Points: %v != %v", exp.Points, act.Points)
+	default:
+		for _, points := range [][]SketchPoint{exp.Points, act.Points} {
+			sort.SliceStable(points, func(i, j int) bool {
+				return points[i].Ts < points[j].Ts
+			})
+		}
+
+		assert.Equal(t, exp.Points, act.Points)
+
+		// assert.Equal does lots of magic, lets double check with a concrete equals
+		// method.
+		for i := range exp.Points {
+			a, e := act.Points[i], exp.Points[i]
+			if a.Ts != e.Ts || !a.Sketch.Equals(e.Sketch) {
+				t.Errorf("Points[%d]: %s != %s", a, e)
+			}
+		}
+	}
+}
+
+type tHelper interface {
+	Helper()
+}
