2f0dd23edcd1dce6bdfb6e00b64caf6d1e8baae6
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:10:17 -0400

    Initialize arbll/6.10-go1.10

---
+++ pkg/trace/event/extractor_legacy.go
@@ -0,0 +1,36 @@
+package event
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/trace/pb"
+	"github.com/DataDog/datadog-agent/pkg/trace/sampler"
+	"github.com/DataDog/datadog-agent/pkg/trace/traceutil"
+)
+
+// legacyExtractor is an event extractor that decides whether to extract APM events from spans based on
+// `serviceName => sampling rate` mappings.
+type legacyExtractor struct {
+	rateByService map[string]float64
+}
+
+// NewLegacyExtractor returns an APM event extractor that decides whether to extract APM events from spans following the
+// specified extraction rates for a span's service.
+func NewLegacyExtractor(rateByService map[string]float64) Extractor {
+	return &legacyExtractor{
+		rateByService: rateByService,
+	}
+}
+
+// Extract decides to extract an apm event from the provided span if there's an extraction rate configured for that
+// span's service. In this case the extracted event is returned along with the found extraction rate and a true value.
+// If this rate doesn't exist or the provided span is not a top level one, then no extraction is done and false is
+// returned as the third value, with the others being invalid.
+func (e *legacyExtractor) Extract(s *pb.Span, priority sampler.SamplingPriority) (float64, bool) {
+	if !traceutil.HasTopLevel(s) {
+		return 0, false
+	}
+	extractionRate, ok := e.rateByService[s.Service]
+	if !ok {
+		return 0, false
+	}
+	return extractionRate, true
+}
