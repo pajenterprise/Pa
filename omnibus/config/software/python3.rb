30ac439c305bda6764070b4e7995d3796ad0038d
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:26:19 -0400

    Initialize albertvaka/go-get-version

---
+++ omnibus/config/software/python3.rb
@@ -0,0 +1,79 @@
+name "python3"
+
+default_version "3.7.6"
+
+if ohai["platform"] != "windows"
+  dependency "libffi"
+  dependency "ncurses"
+  dependency "zlib"
+  dependency "openssl"
+  dependency "bzip2"
+  dependency "libsqlite3"
+  dependency "liblzma"
+  dependency "libyaml"
+
+  source :url => "https://python.org/ftp/python/#{version}/Python-#{version}.tgz",
+         :sha256 => "aeee681c235ad336af116f08ab6563361a0c81c537072c1b309d6e4050aa2114"
+
+  relative_path "Python-#{version}"
+
+  python_configure = ["./configure",
+                      "--prefix=#{install_dir}/embedded"]
+
+  if mac_os_x?
+    python_configure.push("--enable-ipv6",
+                          "--with-universal-archs=intel",
+                          "--enable-shared")
+  elsif linux?
+    python_configure.push("--enable-shared",
+                          "--enable-ipv6")
+  elsif aix?
+    # something here...
+  end
+
+  python_configure.push("--with-dbmliborder=")
+
+  build do
+    ship_license "PSFL"
+
+    env = case ohai["platform"]
+          when "aix"
+            aix_env
+          else
+            {
+              "CFLAGS" => "-I#{install_dir}/embedded/include -O2 -g -pipe",
+              "LDFLAGS" => "-Wl,-rpath,#{install_dir}/embedded/lib -L#{install_dir}/embedded/lib",
+            }
+          end
+    command python_configure.join(" "), :env => env
+    command "make -j #{workers}", :env => env
+    command "make install", :env => env
+    delete "#{install_dir}/embedded/lib/python3.7/test"
+
+    # There exists no configure flag to tell Python to not compile readline support :(
+    major, minor, bugfix = version.split(".")
+    block do
+      FileUtils.rm_f(Dir.glob("#{install_dir}/embedded/lib/python#{major}.#{minor}/lib-dynload/readline.*"))
+      FileUtils.rm_f(Dir.glob("#{install_dir}/embedded/lib/python#{major}.#{minor}/distutils/command/wininst-*.exe"))
+    end
+  end
+
+else
+  dependency "vc_redist_14"
+
+  if windows_arch_i386?
+    dependency "vc_ucrt_redist"
+
+    source :url => "http://s3.amazonaws.com/dd-agent-omnibus/python-windows-#{version}-x86.zip",
+            :sha256 => "946335d0f15e0c4c2820b5ce1ecd4b6470b8f6f28b774d4572279f2d0478c8ab"
+  else
+
+    # note that startring with 3.7.3 on Windows, the zip should be created without the built-in pip
+    source :url => "https://s3.amazonaws.com/dd-agent-omnibus/python-windows-#{version}-amd64.zip",
+         :sha256 => "618325664cadaa80766a3c89f480904bbf23fcbb61467ec36c5d87be026c00cf"
+
+  end
+  build do
+    command "XCOPY /YEHIR *.* \"#{windows_safe_path(python_3_embedded)}\""
+  end
+end
