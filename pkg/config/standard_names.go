98df377c75ed73fbc00ef0f6f68a52c90fe28ea3
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:41:37 -0400

    Initialize kserrania/choco-fix-aws-routing

---
+++ pkg/config/standard_names.go
@@ -0,0 +1,40 @@
+package config
+
+// StandardJMXIntegrations is the list of standard jmx integrations
+var StandardJMXIntegrations = map[string]struct{}{
+	"activemq":    {},
+	"activemq_58": {},
+	"cassandra":   {},
+	"jmx":         {},
+	"presto":      {},
+	"solr":        {},
+	"tomcat":      {},
+	"kafka":       {},
+}
+
+// StandardStatsdPrefixes is a list of the statsd prefixes used by the agent and its components
+var StandardStatsdPrefixes = []string{
+	"datadog.agent",
+	"datadog.dogstatsd",
+	"datadog.process",
+	"datadog.trace_agent",
+	"datadog.tracer",
+
+	"activemq",
+	"activemq_58",
+	"airflow",
+	"cassandra",
+	"confluent",
+	"hazelcast",
+	"hive",
+	"ignite",
+	"jboss",
+	"jvm",
+	"kafka",
+	"presto",
+	"sidekiq",
+	"solr",
+	"tomcat",
+
+	"runtime",
+}
