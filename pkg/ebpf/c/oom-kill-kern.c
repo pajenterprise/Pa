fa78b4eae93f2ed1d8f16f98f604bd36ea59208e
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:50:47 -0400

    Initialize celene/oom_probe_experimental

---
+++ pkg/ebpf/c/oom-kill-kern.c
@@ -0,0 +1,88 @@
+#define KBUILD_MODNAME "foo"
+// #include <linux/bpf.h>
+// #include <linux/tcp.h>
+#include <linux/oom.h>
+#include "bpf-common.h"
+// #include "oom-kill-kern-user.h"
+// #include <linux/oom.h>
+// #include <linux/sched.h>
+
+
+/*
+ * The `oomStats` hash map is used to share with the userland program system-probe
+ * the statistics per pid
+ */
+BPF_HASH(oomStats, u32, struct oom_stats);
+
+static inline bool is_memcg_oom(struct oom_control *oc)
+{
+  return oc->memcg != NULL;
+}
+
+// static inline int set_cgroup_name(char *buf, size_t sz) {
+//     struct task_struct *cur_tskd = (struct task_struct *)bpf_get_current_task();
+//     struct css_set *css_set;
+//     if (!bpf_probe_read(&css_set, sizeof(css_set), &cur_tskd->cgroups)) {
+//       struct cgroup_subsys_state *css;
+//       // TODO: Do not arbitrarily pick the first subsystem
+//       if (!bpf_probe_read(&css, sizeof(css), &css_set->subsys[0])) {
+//         struct cgroup *cgrp;
+//         if (!bpf_probe_read(&cgrp, sizeof(cgrp), &css->cgroup)) {
+//           struct kernfs_node *kn;
+//           if (!bpf_probe_read(&kn, sizeof(kn), &cgrp->kn)) {
+//             if (!bpf_probe_read(buf, sz, &kn->name)) {
+//               return 0;
+//             }
+//           }
+//         }
+//       }
+//     }
+//     return -1;
+// }
+
+int kprobe__oom_kill_process(struct pt_regs *ctx, struct oom_control *oc, const char *message) {
+    struct oom_stats zero = {};
+    u32 pid = bpf_get_current_pid_tgid() >> 32;
+
+    struct oom_stats *s = oomStats.lookup_or_init(&pid, &zero);
+    if (s == NULL) return 0;
+
+    set_cgroup_name(s->cgroup_name, sizeof(s->cgroup_name));
+
+    // // get cgroup name
+    // struct task_struct *cur_tsk = (struct task_struct *)bpf_get_current_task();
+    // struct css_set *css_set;
+    // if (!bpf_probe_read(&css_set, sizeof(css_set), &cur_tsk->cgroups)) {
+    //   struct cgroup_subsys_state *css;
+    //   // TODO: Do not arbitrarily pick the first subsystem
+    //   if (!bpf_probe_read(&css, sizeof(css), &css_set->subsys[0])) {
+    //     struct cgroup *cgrp;
+    //     if (!bpf_probe_read(&cgrp, sizeof(cgrp), &css->cgroup)) {
+    //       struct kernfs_node *kn;
+    //       if (!bpf_probe_read(&kn, sizeof(kn), &cgrp->kn)) {
+    //         const char *name;
+    //         if (!bpf_probe_read(&name, sizeof(name), &kn->name)) {
+    //           bpf_probe_read_str(&s->cgroup_name, sizeof(s->cgroup_name), name);
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
+
+    struct task_struct *p = oc->chosen;
+    unsigned long totalpages;
+
+    s->pid = pid;
+    s->tpid = p->pid;
+    bpf_get_current_comm(&s->fcomm, sizeof(s->fcomm));
+    bpf_probe_read(&s->tcomm, sizeof(s->tcomm), p->comm);
+    s->pages = oc->totalpages;
+
+    #if LINUX_VERSION_CODE >= KERNEL_VERSION(4,9,0)
+      s->memcg_oom = is_memcg_oom(oc) ? 1 : 0;
+    #elif LINUX_VERSION_CODE >= KERNEL_VERSION(4,8,0)
+      s->memcg_oom = oc->memcg != NULL ? 1 : 0;
+    #endif
+
+    return 0;
+}
