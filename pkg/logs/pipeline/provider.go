b67cc74507763a41e38e935768a9c8f4b70c9981
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:05:08 -0400

    Initialize massi/reqfile

---
+++ pkg/logs/pipeline/provider.go
@@ -0,0 +1,72 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2018 Datadog, Inc.
+
+package pipeline
+
+import (
+	"sync/atomic"
+
+	"github.com/DataDog/datadog-agent/pkg/logs/message"
+	"github.com/DataDog/datadog-agent/pkg/logs/restart"
+	"github.com/DataDog/datadog-agent/pkg/logs/sender"
+)
+
+// Provider provides message channels
+type Provider interface {
+	Start()
+	Stop()
+	NextPipelineChan() chan message.Message
+}
+
+// provider implements providing logic
+type provider struct {
+	numberOfPipelines    int
+	connManager          *sender.ConnectionManager
+	outputChan           chan message.Message
+	pipelines            []*Pipeline
+	currentPipelineIndex int32
+}
+
+// NewProvider returns a new Provider
+func NewProvider(numberOfPipelines int, connManager *sender.ConnectionManager, outputChan chan message.Message) Provider {
+	return &provider{
+		numberOfPipelines: numberOfPipelines,
+		connManager:       connManager,
+		outputChan:        outputChan,
+		pipelines:         []*Pipeline{},
+	}
+}
+
+// Start initializes the pipelines
+func (p *provider) Start() {
+	for i := 0; i < p.numberOfPipelines; i++ {
+		pipeline := NewPipeline(p.connManager, p.outputChan)
+		pipeline.Start()
+		p.pipelines = append(p.pipelines, pipeline)
+	}
+}
+
+// Stop stops all pipelines in parallel,
+// this call blocks until all pipelines are stopped
+func (p *provider) Stop() {
+	stopper := restart.NewParallelStopper()
+	for _, pipeline := range p.pipelines {
+		stopper.Add(pipeline)
+	}
+	stopper.Stop()
+	p.pipelines = p.pipelines[:0]
+}
+
+// NextPipelineChan returns the next pipeline input channel
+func (p *provider) NextPipelineChan() chan message.Message {
+	pipelinesLen := len(p.pipelines)
+	if pipelinesLen == 0 {
+		return nil
+	}
+	index := int(p.currentPipelineIndex+1) % pipelinesLen
+	defer atomic.StoreInt32(&p.currentPipelineIndex, int32(index))
+	nextPipeline := p.pipelines[index]
+	return nextPipeline.InputChan
+}
