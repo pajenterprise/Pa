0418799d439237011cfa79b6bceb6efd1427ab32
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:08:05 -0400

    Initialize db/source_test_win

---
+++ pkg/logs/client/destination.go
@@ -0,0 +1,76 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2018 Datadog, Inc.
+
+package client
+
+import (
+	"net"
+)
+
+// FramingError represents a kind of error that can occur when a log can not properly
+// be transformed into a frame.
+type FramingError struct {
+	err error
+}
+
+// NewFramingError returns a new framing error.
+func NewFramingError(err error) *FramingError {
+	return &FramingError{
+		err: err,
+	}
+}
+
+// Error returns the message of the error.
+func (e *FramingError) Error() string {
+	return e.err.Error()
+}
+
+// Destination is responsible for shipping logs to a remote server over TCP.
+type Destination struct {
+	prefixer            Prefixer
+	delimiter           Delimiter
+	connManager         *ConnectionManager
+	destinationsContext *DestinationsContext
+	conn                net.Conn
+}
+
+// NewDestination returns a new destination.
+func NewDestination(endpoint Endpoint, destinationsContext *DestinationsContext) *Destination {
+	return &Destination{
+		prefixer:            NewAPIKeyPrefixer(endpoint.APIKey, endpoint.Logset),
+		delimiter:           NewDelimiter(endpoint.UseProto),
+		connManager:         NewConnectionManager(endpoint),
+		destinationsContext: destinationsContext,
+	}
+}
+
+// Send transforms a message into a frame and sends it to a remote server,
+// returns an error if the operation failed.
+func (d *Destination) Send(payload []byte) error {
+	if d.conn == nil {
+		var err error
+
+		// We work only if we have a started destination context
+		ctx := d.destinationsContext.Context()
+		if d.conn, err = d.connManager.NewConnection(ctx); err != nil {
+			return err
+		}
+	}
+
+	content := d.prefixer.prefix(payload)
+	frame, err := d.delimiter.delimit(content)
+	if err != nil {
+		return NewFramingError(err)
+	}
+
+	_, err = d.conn.Write(frame)
+	if err != nil {
+		d.connManager.CloseConnection(d.conn)
+		d.conn = nil
+		return err
+	}
+
+	return nil
+}
