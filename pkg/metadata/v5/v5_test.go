575018fe3614fc9b2e1d32a2489b49126721f27b
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:17:15 -0400

    Initialize olivielpeau/strip-build-true

---
+++ pkg/metadata/v5/v5_test.go
@@ -0,0 +1,17 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2019 Datadog, Inc.
+
+package v5
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetPayload(t *testing.T) {
+	pl := GetPayload("testhostname")
+	assert.NotNil(t, pl)
+}
