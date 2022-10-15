63b5ce824f0695903ede074dc3bf39bcebc14cb4
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 22:17:31 -0400

    Initialize cc/patch-gstatus

---
+++ omnibus/config/software/gstatus-py2.rb
@@ -0,0 +1,19 @@
+
+name "gstatus-py2"
+#dependency "pip2"
+dependency "python2"
+
+default_version "1.0.4"
+relative_path "gstatus-#{version}"
+
+source :url => "https://github.com/gluster/gstatus/archive/v#{version}.tar.gz",
+       :sha256 => "774e219c0cba8c38d2731938cd10db76d3371d744c2e4cde54c32de95a179184",
+       :extract => :seven_zip
+
+build do
+  ship_license "https://raw.githubusercontent.com/gluster/gstatus/v#{version}/LICENSE"
+  command "make gen-version", :env => {"VERSION" => "#{version}"}
+  python_path = "#{install_dir}/embedded/bin/python"
+  # command "#{install_dir}/embedded/bin/pip2 install ."
+  command "#{python_path} setup.py install"
+end
