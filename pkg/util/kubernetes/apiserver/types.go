d0aead8c6748bdc2ffd602a7559cec6e93d6b876
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:25:29 -0400

    Initialize lee.avital/conntrack_logging

---
+++ pkg/util/kubernetes/apiserver/types.go
@@ -0,0 +1,19 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2020 Datadog, Inc.
+
+// +build kubeapiserver
+
+package apiserver
+
+// LeaderElectorInterface is the interface avoiding the import cycle between the LeaderElection and the APIServer
+type LeaderElectorInterface interface {
+	IsLeader() bool
+}
+
+const (
+	autoscalerNowHandleMsgEvent = "Autoscaler is now handle by the Cluster-Agent"
+	autoscalerIgnoreMsgEvent    = "Autoscaler is ignored, to many metrics already handled by the Cluster-Agent"
+	autoscalerUnIgnoreMsgEvent  = "Autoscaler will now be processed by the Cluster-Agent"
+)
