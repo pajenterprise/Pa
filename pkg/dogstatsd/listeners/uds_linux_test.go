46597e3aaaa374496e4c31ca221379ef5bc2064b
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 22:10:37 -0400

    Initialize maxime/secondary-dogstatsd-telemetry

---
+++ pkg/dogstatsd/listeners/uds_linux_test.go
@@ -0,0 +1,45 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2020 Datadog, Inc.
+
+// +build linux
+// Origin detection is linux-only
+
+// Most of it is tested by test/integration/dogstatsd/origin_detection_test.go
+// that requires a docker environment to run
+
+package listeners
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"golang.org/x/sys/unix"
+)
+
+func TestUDSPassCred(t *testing.T) {
+	dir, err := ioutil.TempDir("", "dd-test-")
+	assert.Nil(t, err)
+	defer os.RemoveAll(dir) // clean up
+	socketPath := filepath.Join(dir, "dsd.socket")
+
+	s, err := NewUDSListener(socketPath, true, nil, NewPacketPool(512))
+	defer s.Stop()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, s)
+
+	// Test socket has PASSCRED option set to 1
+	f, err := s.conn.File()
+	defer f.Close()
+	assert.Nil(t, err)
+
+	enabled, err := unix.GetsockoptInt(int(f.Fd()), unix.SOL_SOCKET, unix.SO_PASSCRED)
+	assert.Nil(t, err)
+	assert.Equal(t, enabled, 1)
+}
