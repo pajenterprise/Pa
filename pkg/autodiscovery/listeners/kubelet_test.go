3deedd3c3b8698f5e380bdea5e2e2a963577fc8a
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:07:10 -0400

    Initialize ecs_fargate_backport

---
+++ pkg/autodiscovery/listeners/kubelet_test.go
@@ -0,0 +1,206 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2018 Datadog, Inc.
+
+// +build kubelet
+
+package listeners
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/DataDog/datadog-agent/pkg/config"
+	"github.com/DataDog/datadog-agent/pkg/util/containers"
+	"github.com/DataDog/datadog-agent/pkg/util/kubernetes/kubelet"
+)
+
+func getMockedPods() []*kubelet.Pod {
+	containerSpecs := []kubelet.ContainerSpec{
+		{
+			Name:  "foo",
+			Image: "datadoghq.com/foo:latest",
+			Ports: []kubelet.ContainerPortSpec{
+				// test that resolved ports are sorted in ascending order
+				{
+					ContainerPort: 1339,
+					HostPort:      1340,
+					Name:          "fooudpport",
+					Protocol:      "UDP",
+				},
+				{
+					ContainerPort: 1337,
+					HostPort:      1338,
+					Name:          "footcpport",
+					Protocol:      "TCP",
+				},
+			},
+		},
+		{
+			Name:  "bar",
+			Image: "datadoghq.com/bar:latest",
+			Ports: []kubelet.ContainerPortSpec{
+				{
+					ContainerPort: 1122,
+					HostPort:      1133,
+					Name:          "barport",
+					Protocol:      "TCP",
+				},
+			},
+		},
+		{
+			Name:  "baz",
+			Image: "datadoghq.com/baz:latest",
+			Ports: []kubelet.ContainerPortSpec{
+				{
+					ContainerPort: 1122,
+					HostPort:      1133,
+					Name:          "barport",
+					Protocol:      "TCP",
+				},
+			},
+		},
+		{
+			Name:  "excluded",
+			Image: "datadoghq.com/baz:latest",
+			Ports: []kubelet.ContainerPortSpec{
+				{
+					ContainerPort: 1122,
+					HostPort:      1133,
+					Name:          "barport",
+					Protocol:      "TCP",
+				},
+			},
+		},
+	}
+	kubeletSpec := kubelet.Spec{
+		HostNetwork: false,
+		NodeName:    "mockn-node",
+		Containers:  containerSpecs,
+	}
+	containerStatuses := []kubelet.ContainerStatus{
+		{
+			Name:  "foo",
+			Image: "datadoghq.com/foo:latest",
+			ID:    "docker://foorandomhash",
+		},
+		{
+			Name:  "bar",
+			Image: "datadoghq.com/bar:latest",
+			ID:    "rkt://bar-random-hash",
+		},
+		{
+			Name:  "baz",
+			Image: "datadoghq.com/baz:latest",
+			ID:    "docker://containerid",
+		},
+		{
+			Name:  "excluded",
+			Image: "datadoghq.com/baz:latest",
+			ID:    "docker://excluded",
+		},
+	}
+	kubeletStatus := kubelet.Status{
+		Phase:      "Running",
+		PodIP:      "127.0.0.1",
+		HostIP:     "127.0.0.2",
+		Containers: containerStatuses,
+	}
+	return []*kubelet.Pod{
+		{
+			Spec:   kubeletSpec,
+			Status: kubeletStatus,
+			Metadata: kubelet.PodMetadata{
+				Name: "mock-pod",
+				Annotations: map[string]string{
+					"ad.datadoghq.com/baz.instances": "[]",
+				},
+			},
+		},
+	}
+}
+
+func TestProcessNewPod(t *testing.T) {
+	config.Datadog.SetDefault("ac_include", []string{"name:baz"})
+	config.Datadog.SetDefault("ac_exclude", []string{"image:datadoghq.com/baz.*"})
+	containers.ResetSharedFilter()
+
+	defer func() {
+		config.Datadog.SetDefault("ac_include", []string{})
+		config.Datadog.SetDefault("ac_exclude", []string{})
+		containers.ResetSharedFilter()
+	}()
+
+	services := make(chan Service, 5)
+	listener := KubeletListener{
+		newService: services,
+		services:   make(map[string]Service),
+	}
+	listener.filter, _ = containers.GetSharedFilter()
+
+	listener.processNewPods(getMockedPods(), false)
+
+	select {
+	case service := <-services:
+		assert.Equal(t, "docker://foorandomhash", string(service.GetEntity()))
+		adIdentifiers, err := service.GetADIdentifiers()
+		assert.Nil(t, err)
+		assert.Equal(t, []string{"docker://foorandomhash", "datadoghq.com/foo:latest", "foo"}, adIdentifiers)
+		hosts, err := service.GetHosts()
+		assert.Nil(t, err)
+		assert.Equal(t, map[string]string{"pod": "127.0.0.1"}, hosts)
+		ports, err := service.GetPorts()
+		assert.Nil(t, err)
+		assert.Equal(t, []ContainerPort{{1337, "footcpport"}, {1339, "fooudpport"}}, ports)
+		_, err = service.GetPid()
+		assert.Equal(t, ErrNotSupported, err)
+	default:
+		assert.FailNow(t, "first service not in channel")
+	}
+
+	select {
+	case service := <-services:
+		assert.Equal(t, "rkt://bar-random-hash", string(service.GetEntity()))
+		adIdentifiers, err := service.GetADIdentifiers()
+		assert.Nil(t, err)
+		assert.Equal(t, []string{"rkt://bar-random-hash", "datadoghq.com/bar:latest", "bar"}, adIdentifiers)
+		hosts, err := service.GetHosts()
+		assert.Nil(t, err)
+		assert.Equal(t, map[string]string{"pod": "127.0.0.1"}, hosts)
+		ports, err := service.GetPorts()
+		assert.Nil(t, err)
+		assert.Equal(t, []ContainerPort{{1122, "barport"}}, ports)
+		_, err = service.GetPid()
+		assert.Equal(t, ErrNotSupported, err)
+	default:
+		assert.FailNow(t, "second service not in channel")
+	}
+
+	select {
+	case service := <-services:
+		assert.Equal(t, "docker://containerid", string(service.GetEntity()))
+		adIdentifiers, err := service.GetADIdentifiers()
+		assert.Nil(t, err)
+		assert.Equal(t, []string{"docker://containerid"}, adIdentifiers)
+		hosts, err := service.GetHosts()
+		assert.Nil(t, err)
+		assert.Equal(t, map[string]string{"pod": "127.0.0.1"}, hosts)
+		ports, err := service.GetPorts()
+		assert.Nil(t, err)
+		assert.Equal(t, []ContainerPort{{1122, "barport"}}, ports)
+		_, err = service.GetPid()
+		assert.Equal(t, ErrNotSupported, err)
+	default:
+		assert.FailNow(t, "third service not in channel")
+	}
+
+	// Fourth container is filtered out
+	select {
+	case <-services:
+		assert.FailNow(t, "four services in channel, filtering is broken")
+	default:
+		// all good
+	}
+}
