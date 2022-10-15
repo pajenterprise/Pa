88448a4af91d871354bdd0bd4db78b981ebc5d3c
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:07:27 -0400

    Initialize corentin.chary/pythonpath

---
+++ pkg/collector/check/loader.go
@@ -0,0 +1,19 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2018 Datadog, Inc.
+
+package check
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/autodiscovery/integration"
+)
+
+// Loader is the interface wrapping the operations to load a check from
+// different sources, like Python modules or Go objects.
+//
+// A check is loaded for every `instance` found in the configuration file.
+// Load is supposed to break down instances and return different checks.
+type Loader interface {
+	Load(config integration.Config) ([]Check, error)
+}
