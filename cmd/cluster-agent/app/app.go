e025c1c667e2921c4419b363b5e9a1ce94cdf291
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:21:37 -0400

    Initialize arbll/aggr-improve-sort-uniq

---
+++ cmd/cluster-agent/app/app.go
@@ -0,0 +1,264 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2019 Datadog, Inc.
+
+// +build kubeapiserver
+
+package app
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+
+	"github.com/DataDog/datadog-agent/cmd/agent/common"
+	"github.com/DataDog/datadog-agent/cmd/cluster-agent/api"
+	"github.com/DataDog/datadog-agent/cmd/cluster-agent/custommetrics"
+	"github.com/DataDog/datadog-agent/pkg/aggregator"
+	"github.com/DataDog/datadog-agent/pkg/api/healthprobe"
+	"github.com/DataDog/datadog-agent/pkg/clusteragent"
+	"github.com/DataDog/datadog-agent/pkg/clusteragent/clusterchecks"
+	"github.com/DataDog/datadog-agent/pkg/config"
+	"github.com/DataDog/datadog-agent/pkg/forwarder"
+	"github.com/DataDog/datadog-agent/pkg/serializer"
+	"github.com/DataDog/datadog-agent/pkg/status/health"
+	"github.com/DataDog/datadog-agent/pkg/util"
+	"github.com/DataDog/datadog-agent/pkg/util/kubernetes/apiserver"
+	"github.com/DataDog/datadog-agent/pkg/util/kubernetes/apiserver/leaderelection"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+	"github.com/DataDog/datadog-agent/pkg/version"
+)
+
+// loggerName is the name of the cluster agent logger
+const loggerName config.LoggerName = "CLUSTER"
+
+// FIXME: move SetupAutoConfig and StartAutoConfig in their own package so we don't import cmd/agent
+var (
+	ClusterAgentCmd = &cobra.Command{
+		Use:   "datadog-cluster-agent [command]",
+		Short: "Datadog Cluster Agent at your service.",
+		Long: `
+Datadog Cluster Agent takes care of running checks that need run only once per cluster.
+It also exposes an API for other Datadog agents that provides them with cluster-level
+metadata for their metrics.`,
+	}
+
+	startCmd = &cobra.Command{
+		Use:   "start",
+		Short: "Start the Cluster Agent",
+		Long:  `Runs Datadog Cluster agent in the foreground`,
+		RunE:  start,
+	}
+
+	versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Print the version info",
+		Long:  ``,
+		Run: func(cmd *cobra.Command, args []string) {
+			if flagNoColor {
+				color.NoColor = true
+			}
+			av, _ := version.Agent()
+			meta := ""
+			if av.Meta != "" {
+				meta = fmt.Sprintf("- Meta: %s ", color.YellowString(av.Meta))
+			}
+			fmt.Fprintln(
+				color.Output,
+				fmt.Sprintf("Cluster agent %s %s- Commit: '%s' - Serialization version: %s",
+					color.BlueString(av.GetNumberAndPre()),
+					meta,
+					color.GreenString(version.Commit),
+					color.MagentaString(serializer.AgentPayloadVersion),
+				),
+			)
+		},
+	}
+
+	confPath    string
+	flagNoColor bool
+	stopCh      chan struct{}
+)
+
+func init() {
+	// attach the command to the root
+	ClusterAgentCmd.AddCommand(startCmd)
+	ClusterAgentCmd.AddCommand(versionCmd)
+
+	ClusterAgentCmd.PersistentFlags().StringVarP(&confPath, "cfgpath", "c", "", "path to directory containing datadog.yaml")
+	ClusterAgentCmd.PersistentFlags().BoolVarP(&flagNoColor, "no-color", "n", false, "disable color output")
+}
+
+func start(cmd *cobra.Command, args []string) error {
+	// we'll search for a config file named `datadog-cluster.yaml`
+	config.Datadog.SetConfigName("datadog-cluster")
+	err := common.SetupConfig(confPath)
+	if err != nil {
+		return fmt.Errorf("unable to set up global agent configuration: %v", err)
+	}
+	// Setup logger
+	syslogURI := config.GetSyslogURI()
+	logFile := config.Datadog.GetString("log_file")
+	if logFile == "" {
+		logFile = common.DefaultDCALogFile
+	}
+	if config.Datadog.GetBool("disable_file_logging") {
+		// this will prevent any logging on file
+		logFile = ""
+	}
+
+	mainCtx, mainCtxCancel := context.WithCancel(context.Background())
+	defer mainCtxCancel() // Calling cancel twice is safe
+
+	err = config.SetupLogger(
+		loggerName,
+		config.Datadog.GetString("log_level"),
+		logFile,
+		syslogURI,
+		config.Datadog.GetBool("syslog_rfc"),
+		config.Datadog.GetBool("log_to_console"),
+		config.Datadog.GetBool("log_format_json"),
+	)
+	if err != nil {
+		log.Criticalf("Unable to setup logger: %s", err)
+		return nil
+	}
+
+	if !config.Datadog.IsSet("api_key") {
+		log.Critical("no API key configured, exiting")
+		return nil
+	}
+
+	// Setup healthcheck port
+	var healthPort = config.Datadog.GetInt("health_port")
+	if healthPort > 0 {
+		err := healthprobe.Serve(mainCtx, healthPort)
+		if err != nil {
+			return log.Errorf("Error starting health port, exiting: %v", err)
+		}
+		log.Debugf("Health check listening on port %d", healthPort)
+	}
+
+	// get hostname
+	hostname, err := util.GetHostname()
+	if err != nil {
+		return log.Errorf("Error while getting hostname, exiting: %v", err)
+	}
+	log.Infof("Hostname is: %s", hostname)
+
+	// setup the forwarder
+	keysPerDomain, err := config.GetMultipleEndpoints()
+	if err != nil {
+		log.Error("Misconfiguration of agent endpoints: ", err)
+	}
+	f := forwarder.NewDefaultForwarder(keysPerDomain)
+	f.Start()
+	s := serializer.NewSerializer(f)
+
+	aggregatorInstance := aggregator.InitAggregator(s, hostname, "cluster_agent")
+	aggregatorInstance.AddAgentStartupTelemetry(fmt.Sprintf("%s - Datadog Cluster Agent", version.AgentVersion))
+
+	log.Infof("Datadog Cluster Agent is now running.")
+
+	apiCl, err := apiserver.GetAPIClient() // make sure we can connect to the apiserver
+	if err != nil {
+		log.Errorf("Could not connect to the apiserver: %v", err)
+	} else {
+		le, err := leaderelection.GetLeaderEngine()
+		if err != nil {
+			return err
+		}
+		stopCh := make(chan struct{})
+		ctx := apiserver.ControllerContext{
+			InformerFactory:    apiCl.InformerFactory,
+			WPAInformerFactory: apiCl.WPAInformerFactory,
+			Client:             apiCl.Cl,
+			LeaderElector:      le,
+			StopCh:             stopCh,
+		}
+
+		if err := apiserver.StartControllers(ctx); err != nil {
+			log.Errorf("Could not start controllers: %v", err)
+		}
+	}
+
+	// Setup a channel to catch OS signals
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+	// create and setup the Autoconfig instance
+	common.SetupAutoConfig(config.Datadog.GetString("confd_path"))
+	// start the autoconfig, this will immediately run any configured check
+	common.StartAutoConfig()
+
+	// Start the cluster-check discovery if configured
+	clusterCheckHandler := setupClusterCheck(mainCtx)
+	// start the cmd HTTPS server
+	sc := clusteragent.ServerContext{
+		ClusterCheckHandler: clusterCheckHandler,
+	}
+	if err = api.StartServer(sc); err != nil {
+		return log.Errorf("Error while starting api server, exiting: %v", err)
+	}
+	wg := sync.WaitGroup{}
+	// Autoscaler Controller Process
+	if config.Datadog.GetBool("external_metrics_provider.enabled") {
+		// Start the k8s custom metrics server. This is a blocking call
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			errServ := custommetrics.RunServer(mainCtx)
+			if errServ != nil {
+				log.Errorf("Error in the External Metrics API Server: %v", errServ)
+			}
+		}()
+	}
+
+	// Block here until we receive the interrupt signal
+	<-signalCh
+
+	// retrieve the agent health before stopping the components
+	// GetStatusNonBlocking has a 100ms timeout to avoid blocking
+	health, err := health.GetStatusNonBlocking()
+	if err != nil {
+		log.Warnf("Cluster Agent health unknown: %s", err)
+	} else if len(health.Unhealthy) > 0 {
+		log.Warnf("Some components were unhealthy: %v", health.Unhealthy)
+	}
+
+	// Cancel the main context to stop components
+	mainCtxCancel()
+	// wait for the External Metrics Server to stop properly
+	wg.Wait()
+
+	if stopCh != nil {
+		close(stopCh)
+	}
+	log.Info("See ya!")
+	log.Flush()
+	return nil
+}
+
+func setupClusterCheck(ctx context.Context) *clusterchecks.Handler {
+	if !config.Datadog.GetBool("cluster_checks.enabled") {
+		log.Debug("Cluster check Autodiscovery disabled")
+		return nil
+	}
+
+	handler, err := clusterchecks.NewHandler(common.AC)
+	if err != nil {
+		log.Errorf("Could not setup the cluster-checks Autodiscovery: %s", err.Error())
+		return nil
+	}
+	go handler.Run(ctx)
+
+	log.Info("Started cluster check Autodiscovery")
+	return handler
+}
---
+++ pkg/aggregator/context_resolver.go
@@ -0,0 +1,88 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2019 Datadog, Inc.
+
+package aggregator
+
+import (
+	"fmt"
+
+	"github.com/DataDog/datadog-agent/pkg/util"
+
+	"github.com/DataDog/datadog-agent/pkg/aggregator/ckey"
+	"github.com/DataDog/datadog-agent/pkg/metrics"
+)
+
+// Context holds the elements that form a context, and can be serialized into a context key
+type Context struct {
+	Name string
+	Tags []string
+	Host string
+}
+
+// ContextResolver allows tracking and expiring contexts
+type ContextResolver struct {
+	contextsByKey map[ckey.ContextKey]*Context
+	lastSeenByKey map[ckey.ContextKey]float64
+}
+
+func newContextResolver() *ContextResolver {
+	return &ContextResolver{
+		contextsByKey: make(map[ckey.ContextKey]*Context),
+		lastSeenByKey: make(map[ckey.ContextKey]float64),
+	}
+}
+
+// trackContext returns the contextKey associated with the context of the metricSample and tracks that context
+func (cr *ContextResolver) trackContext(metricSampleContext metrics.MetricSampleContext, currentTimestamp float64) ckey.ContextKey {
+	sampleTags := metricSampleContext.GetTags()
+	tags := make([]string, 16)
+	if len(sampleTags) > 16 {
+		tags = make([]string, len(sampleTags))
+	}
+	tags = util.SortUniq(tags, sampleTags)
+	contextKey := ckey.Generate(metricSampleContext.GetName(), metricSampleContext.GetHost(), tags)
+	if _, ok := cr.contextsByKey[contextKey]; !ok {
+		cr.contextsByKey[contextKey] = &Context{
+			Name: metricSampleContext.GetName(),
+			Tags: tags,
+			Host: metricSampleContext.GetHost(),
+		}
+	}
+	cr.lastSeenByKey[contextKey] = currentTimestamp
+
+	return contextKey
+}
+
+// updateTrackedContext updates the last seen timestamp on a given context key
+func (cr *ContextResolver) updateTrackedContext(contextKey ckey.ContextKey, timestamp float64) error {
+	if _, ok := cr.lastSeenByKey[contextKey]; ok && cr.lastSeenByKey[contextKey] < timestamp {
+		cr.lastSeenByKey[contextKey] = timestamp
+	} else if !ok {
+		return fmt.Errorf("Trying to update a context that is not tracked")
+	}
+
+	return nil
+}
+
+// expireContexts cleans up the contexts that haven't been tracked since the given timestamp
+// and returns the associated contextKeys
+func (cr *ContextResolver) expireContexts(expireTimestamp float64) []ckey.ContextKey {
+	var expiredContextKeys []ckey.ContextKey
+
+	// Find expired context keys
+	for contextKey, lastSeen := range cr.lastSeenByKey {
+		if lastSeen < expireTimestamp {
+			expiredContextKeys = append(expiredContextKeys, contextKey)
+		}
+	}
+
+	// Delete expired context keys
+	for _, expiredContextKey := range expiredContextKeys {
+		delete(cr.contextsByKey, expiredContextKey)
+		delete(cr.lastSeenByKey, expiredContextKey)
+	}
+
+	return expiredContextKeys
+}
