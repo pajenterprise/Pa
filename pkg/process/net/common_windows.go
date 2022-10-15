98cbe0e6b46b0294d0ec4a649410bbe8781733f1
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 22:39:21 -0400

    Initialize titouan/whitelist-tags

---
+++ pkg/process/net/common_windows.go
@@ -0,0 +1,19 @@
+// +build windows
+
+package net
+
+import "fmt"
+
+const (
+	connectionsURL = "http://localhost:3333/connections"
+	statsURL       = "http://localhost:3333/debug/stats"
+	netType        = "tcp"
+)
+
+// CheckPath is used to make sure the globalSocketPath has been set before attempting to connect
+func CheckPath() error {
+	if globalSocketPath == "" {
+		return fmt.Errorf("remote tracer has no path defined")
+	}
+	return nil
+}
