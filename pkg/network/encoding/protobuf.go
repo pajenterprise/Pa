46d8c75ceaebb0d8b864e15d0e69f1e22a011735
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 22:16:08 -0400

    Initialize shang/procutil-model-tests

---
+++ pkg/network/encoding/protobuf.go
@@ -0,0 +1,44 @@
+package encoding
+
+import (
+	model "github.com/DataDog/agent-payload/process"
+	"github.com/DataDog/datadog-agent/pkg/network"
+	"github.com/gogo/protobuf/proto"
+)
+
+// ContentTypeProtobuf holds the HTML content-type of a Protobuf payload
+const ContentTypeProtobuf = "application/protobuf"
+
+type protoSerializer struct{}
+
+func (protoSerializer) Marshal(conns *network.Connections) ([]byte, error) {
+	agentConns := make([]*model.Connection, len(conns.Conns))
+
+	for i, conn := range conns.Conns {
+		agentConns[i] = FormatConnection(conn)
+	}
+
+	payload := connsPool.Get().(*model.Connections)
+	payload.Conns = agentConns
+	payload.Dns = FormatDNS(conns.DNS)
+	payload.Telemetry = FormatTelemetry(conns.Telemetry)
+
+	buf, err := proto.Marshal(payload)
+	returnToPool(payload)
+	return buf, err
+}
+
+func (protoSerializer) Unmarshal(blob []byte) (*model.Connections, error) {
+	conns := new(model.Connections)
+	if err := proto.Unmarshal(blob, conns); err != nil {
+		return nil, err
+	}
+	return conns, nil
+}
+
+func (p protoSerializer) ContentType() string {
+	return ContentTypeProtobuf
+}
+
+var _ Marshaler = protoSerializer{}
+var _ Unmarshaler = protoSerializer{}
