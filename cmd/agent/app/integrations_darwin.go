cec7fe347b19aada3f2e7a3a9f02386c1ce33819
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:12:52 -0400

    Initialize kserrania/omnibus-ship-source

---
+++ cmd/agent/app/integrations_darwin.go
@@ -0,0 +1,31 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2019 Datadog, Inc.
+
+// +build python
+
+package app
+
+import (
+	"path/filepath"
+)
+
+const (
+	pythonBin = "python"
+)
+
+var (
+	relPyPath              = filepath.Join("..", "..", "embedded", "bin", pythonBin)
+	relChecksPath          = filepath.Join("..", "..", "embedded", "lib", "python2.7", "site-packages", "datadog_checks")
+	relReqAgentReleasePath = filepath.Join("..", "..", reqAgentReleaseFile)
+	relConstraintsPath     = filepath.Join("..", "..", constraintsFile)
+)
+
+func authorizedUser() bool {
+	return true
+}
+
+func isIntegrationUser() bool {
+	return true
+}
