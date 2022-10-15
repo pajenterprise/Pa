35fc931fc4ea21f21483f9ed648d75758057baeb
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 22:21:13 -0400

    Initialize lenaic/debug-e2e-tests

---
+++ pkg/security/ebpf/c/chown.h
@@ -0,0 +1,128 @@
+#ifndef _CHOWN_H_
+#define _CHOWN_H_
+
+#include "syscalls.h"
+
+struct chown_event_t {
+    struct kevent_t event;
+    struct process_context_t process;
+    struct container_context_t container;
+    struct syscall_t syscall;
+    struct file_t file;
+    uid_t user;
+    gid_t group;
+};
+
+int __attribute__((always_inline)) trace__sys_chown(uid_t user, gid_t group) {
+    struct syscall_cache_t syscall = {
+        .type = SYSCALL_CHOWN,
+        .setattr = {
+            .user = user,
+            .group = group
+        }
+    };
+
+    cache_syscall(&syscall, EVENT_CHOWN);
+
+    if (discarded_by_process(syscall.policy.mode, EVENT_CHOWN)) {
+        pop_syscall(SYSCALL_CHOWN);
+    }
+
+    return 0;
+}
+
+SYSCALL_KPROBE3(lchown, const char*, filename, uid_t, user, gid_t, group) {
+    return trace__sys_chown(user, group);
+}
+
+SYSCALL_KPROBE3(fchown, int, fd, uid_t, user, gid_t, group) {
+    return trace__sys_chown(user, group);
+}
+
+SYSCALL_KPROBE3(chown, const char*, filename, uid_t, user, gid_t, group) {
+    return trace__sys_chown(user, group);
+}
+
+SYSCALL_KPROBE3(lchown16, const char*, filename, uid_t, user, gid_t, group) {
+    return trace__sys_chown(user, group);
+}
+
+SYSCALL_KPROBE3(fchown16, int, fd, uid_t, user, gid_t, group) {
+    return trace__sys_chown(user, group);
+}
+
+SYSCALL_KPROBE3(chown16, const char*, filename, uid_t, user, gid_t, group) {
+    return trace__sys_chown(user, group);
+}
+
+SYSCALL_KPROBE4(fchownat, int, dirfd, const char*, filename, uid_t, user, gid_t, group) {
+    return trace__sys_chown(user, group);
+}
+
+int __attribute__((always_inline)) trace__sys_chown_ret(struct pt_regs *ctx) {
+    struct syscall_cache_t *syscall = pop_syscall(SYSCALL_CHOWN);
+    if (!syscall)
+        return 0;
+
+    int retval = PT_REGS_RC(ctx);
+    if (IS_UNHANDLED_ERROR(retval))
+        return 0;
+
+    // add an real entry to reach the first dentry with the proper inode
+    u64 inode = syscall->setattr.path_key.ino;
+    if (syscall->setattr.real_inode) {
+        inode = syscall->setattr.real_inode;
+        link_dentry_inode(syscall->setattr.path_key, inode);
+    }
+
+    struct chown_event_t event = {
+        .syscall.retval = retval,
+        .file = {
+            .inode = inode,
+            .mount_id = syscall->setattr.path_key.mount_id,
+            .overlay_numlower = get_overlay_numlower(syscall->setattr.dentry),
+            .path_id = syscall->setattr.path_key.path_id,
+        },
+        .user = syscall->setattr.user,
+        .group = syscall->setattr.group,
+    };
+
+    struct proc_cache_t *entry = fill_process_context(&event.process);
+    fill_container_context(entry, &event.container);
+
+    // dentry resolution in setattr.h
+
+    send_event(ctx, EVENT_CHOWN, event);
+
+    return 0;
+}
+
+SYSCALL_KRETPROBE(lchown) {
+    return trace__sys_chown_ret(ctx);
+}
+
+SYSCALL_KRETPROBE(fchown) {
+    return trace__sys_chown_ret(ctx);
+}
+
+SYSCALL_KRETPROBE(chown) {
+    return trace__sys_chown_ret(ctx);
+}
+
+SYSCALL_KRETPROBE(lchown16) {
+    return trace__sys_chown_ret(ctx);
+}
+
+SYSCALL_KRETPROBE(fchown16) {
+    return trace__sys_chown_ret(ctx);
+}
+
+SYSCALL_KRETPROBE(chown16) {
+    return trace__sys_chown_ret(ctx);
+}
+
+SYSCALL_KRETPROBE(fchownat) {
+    return trace__sys_chown_ret(ctx);
+}
+
+#endif
---
+++ pkg/security/ebpf/c/rename.h
@@ -0,0 +1,137 @@
+#ifndef _RENAME_H_
+#define _RENAME_H_
+
+#include "syscalls.h"
+
+struct rename_event_t {
+    struct kevent_t event;
+    struct process_context_t process;
+    struct container_context_t container;
+    struct syscall_t syscall;
+    struct file_t old;
+    struct file_t new;
+};
+
+int __attribute__((always_inline)) trace__sys_rename() {
+    struct syscall_cache_t syscall = {
+        .type = SYSCALL_RENAME,
+    };
+
+    cache_syscall(&syscall, EVENT_RENAME);
+
+    return 0;
+}
+
+SYSCALL_KPROBE0(rename) {
+    return trace__sys_rename();
+}
+
+SYSCALL_KPROBE0(renameat) {
+    return trace__sys_rename();
+}
+
+SYSCALL_KPROBE0(renameat2) {
+    return trace__sys_rename();
+}
+
+SEC("kprobe/vfs_rename")
+int kprobe__vfs_rename(struct pt_regs *ctx) {
+    struct syscall_cache_t *syscall = peek_syscall(SYSCALL_RENAME);
+    if (!syscall)
+        return 0;
+
+    struct dentry *dentry = (struct dentry *)PT_REGS_PARM2(ctx);
+    struct dentry *target_dentry = (struct dentry *)PT_REGS_PARM4(ctx);
+    syscall->rename.target_key.ino = get_dentry_ino(target_dentry);
+
+    // if second pass, ex: overlayfs, just cache the inode that will be used in ret
+    if (syscall->rename.src_dentry) {
+        syscall->rename.real_src_dentry = dentry;
+        return 0;
+    }
+
+    syscall->rename.src_dentry = dentry;
+    syscall->rename.src_overlay_numlower = get_overlay_numlower(syscall->rename.src_dentry);
+
+    // we generate a fake source key as the inode is (can be ?) reused
+    syscall->rename.src_key.ino = FAKE_INODE_MSW<<32 | bpf_get_prandom_u32();
+
+    // the mount id of path_key is resolved by kprobe/mnt_want_write. It is already set by the time we reach this probe.
+    resolve_dentry(syscall->rename.src_dentry, syscall->rename.src_key, 0);
+
+    return 0;
+}
+
+int __attribute__((always_inline)) trace__sys_rename_ret(struct pt_regs *ctx) {
+    struct syscall_cache_t *syscall = pop_syscall(SYSCALL_RENAME);
+    if (!syscall)
+        return 0;
+
+    int retval = PT_REGS_RC(ctx);
+
+    // invalidate non ovl inode, case of folder renamed
+    invalidate_inode(ctx, syscall->rename.target_key.mount_id, get_dentry_ino(syscall->rename.src_dentry), 1);
+
+    // we invalidate the inode of the overridden file
+    if (syscall->rename.target_key.ino) {
+        invalidate_inode(ctx, syscall->rename.target_key.mount_id, syscall->rename.target_key.ino, 1);
+    }
+
+    // Warning: we use the src_dentry twice for compatibility with CentOS. Do not change it :)
+    // (the mount id was set by kprobe/mnt_want_write)
+    syscall->rename.target_key.ino = get_dentry_ino(syscall->rename.src_dentry);
+    if (syscall->rename.real_src_dentry) {
+        syscall->rename.target_key.ino = get_dentry_ino(syscall->rename.real_src_dentry);
+    }
+
+    // If we are discarded, we still want to invalidate the inode
+    if (discarded_by_process(syscall->policy.mode, EVENT_RENAME) || (IS_UNHANDLED_ERROR(retval))) {
+        invalidate_inode(ctx, syscall->rename.target_key.mount_id, syscall->rename.target_key.ino, 1);
+        return 0;
+    }
+
+    int enabled = is_event_enabled(EVENT_RENAME);
+    if (enabled) {
+        syscall->rename.target_key.path_id = get_path_id(1);
+
+        struct rename_event_t event = {
+            .syscall.retval = retval,
+            .old = {
+                .inode = syscall->rename.src_key.ino,
+                .mount_id = syscall->rename.src_key.mount_id,
+                .overlay_numlower = syscall->rename.src_overlay_numlower,
+            },
+            .new = {
+                .inode = syscall->rename.target_key.ino,
+                .mount_id = syscall->rename.target_key.mount_id,
+                .overlay_numlower = get_overlay_numlower(syscall->rename.src_dentry),
+                .path_id = syscall->rename.target_key.path_id,
+            }
+        };
+
+        struct proc_cache_t *entry = fill_process_context(&event.process);
+        fill_container_context(entry, &event.container);
+
+        resolve_dentry(syscall->rename.src_dentry, syscall->rename.target_key, 0);
+
+        send_event(ctx, EVENT_RENAME, event);
+    }
+
+    invalidate_inode(ctx, syscall->rename.target_key.mount_id, syscall->rename.target_key.ino, !enabled);
+
+    return 0;
+}
+
+SYSCALL_KRETPROBE(rename) {
+    return trace__sys_rename_ret(ctx);
+}
+
+SYSCALL_KRETPROBE(renameat) {
+    return trace__sys_rename_ret(ctx);
+}
+
+SYSCALL_KRETPROBE(renameat2) {
+    return trace__sys_rename_ret(ctx);
+}
+
+#endif
