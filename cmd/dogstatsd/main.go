cd401a559ec43be17ee994b163c2f4d982b2f79d
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:31:51 -0400

    Initialize vboulineau/sync_doc_win

---
+++ cmd/dogstatsd/main.go
@@ -0,0 +1,237 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2020 Datadog, Inc.
+
+//go:generate go run ../../pkg/config/render_config.go dogstatsd ../../pkg/config/config_template.yaml ./dist/dogstatsd.yaml
+
+package main
+
+import (
+	"context"
+	_ "expvar"
+	"fmt"
+	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/DataDog/datadog-agent/pkg/metrics"
+
+	"github.com/spf13/cobra"
+
+	"github.com/DataDog/datadog-agent/pkg/aggregator"
+	"github.com/DataDog/datadog-agent/pkg/api/healthprobe"
+	"github.com/DataDog/datadog-agent/pkg/config"
+	"github.com/DataDog/datadog-agent/pkg/dogstatsd"
+	"github.com/DataDog/datadog-agent/pkg/forwarder"
+	"github.com/DataDog/datadog-agent/pkg/metadata"
+	"github.com/DataDog/datadog-agent/pkg/serializer"
+	"github.com/DataDog/datadog-agent/pkg/status/health"
+	"github.com/DataDog/datadog-agent/pkg/tagger"
+	"github.com/DataDog/datadog-agent/pkg/util"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+	"github.com/DataDog/datadog-agent/pkg/version"
+)
+
+var (
+	// dogstatsdCmd is the root command
+	dogstatsdCmd = &cobra.Command{
+		Use:   "dogstatsd [command]",
+		Short: "Datadog dogstatsd at your service.",
+		Long: `
+DogStatsD accepts custom application metrics points over UDP, and then
+periodically aggregates and forwards them to Datadog, where they can be graphed
+on dashboards. DogStatsD implements the StatsD protocol, along with a few
+extensions for special Datadog features.`,
+	}
+
+	startCmd = &cobra.Command{
+		Use:   "start",
+		Short: "Start DogStatsD",
+		Long:  `Runs DogStatsD in the foreground`,
+		RunE:  start,
+	}
+
+	versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number",
+		Long:  ``,
+		Run: func(cmd *cobra.Command, args []string) {
+			av, _ := version.Agent()
+			fmt.Println(fmt.Sprintf("DogStatsD from Agent %s - Codename: %s - Commit: %s - Serialization version: %s - Go version: %s",
+				av.GetNumber(), av.Meta, av.Commit, serializer.AgentPayloadVersion, runtime.Version()))
+		},
+	}
+
+	confPath   string
+	socketPath string
+
+	metaScheduler *metadata.Scheduler
+	statsd        *dogstatsd.Server
+)
+
+const (
+	// loggerName is the name of the dogstatsd logger
+	loggerName config.LoggerName = "DSD"
+)
+
+func init() {
+	// attach the command to the root
+	dogstatsdCmd.AddCommand(startCmd)
+	dogstatsdCmd.AddCommand(versionCmd)
+
+	// local flags
+	startCmd.Flags().StringVarP(&confPath, "cfgpath", "c", "", "path to folder containing dogstatsd.yaml")
+	config.Datadog.BindPFlag("conf_path", startCmd.Flags().Lookup("cfgpath"))
+	startCmd.Flags().StringVarP(&socketPath, "socket", "s", "", "listen to this socket instead of UDP")
+	config.Datadog.BindPFlag("dogstatsd_socket", startCmd.Flags().Lookup("socket"))
+}
+
+func start(cmd *cobra.Command, args []string) error {
+
+	mainCtx, mainCtxCancel, err := runAgent()
+	if err != nil {
+		return err
+	}
+	// Setup a channel to catch OS signals
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+
+	// Block here until we receive the interrupt signal
+	<-signalCh
+
+	stopAgent(mainCtx, mainCtxCancel)
+	return nil
+}
+
+func runAgent() (mainCtx context.Context, mainCtxCancel context.CancelFunc, err error) {
+	// Main context passed to components
+	mainCtx, mainCtxCancel = context.WithCancel(context.Background())
+
+	configFound := false
+
+	// a path to the folder containing the config file was passed
+	if len(confPath) != 0 {
+		// we'll search for a config file named `dogstastd.yaml`
+		config.Datadog.SetConfigName("dogstatsd")
+		config.Datadog.AddConfigPath(confPath)
+		confErr := config.Load()
+		if confErr != nil {
+			log.Error(confErr)
+		} else {
+			configFound = true
+		}
+	}
+
+	if !configFound {
+		log.Infof("Config will be read from env variables")
+	}
+
+	// Setup logger
+	syslogURI := config.GetSyslogURI()
+	logFile := config.Datadog.GetString("log_file")
+	if logFile == "" {
+		logFile = defaultLogFile
+	}
+
+	if config.Datadog.GetBool("disable_file_logging") {
+		// this will prevent any logging on file
+		logFile = ""
+	}
+
+	err = config.SetupLogger(
+		loggerName,
+		config.Datadog.GetString("log_level"),
+		logFile,
+		syslogURI,
+		config.Datadog.GetBool("syslog_rfc"),
+		config.Datadog.GetBool("log_to_console"),
+		config.Datadog.GetBool("log_format_json"),
+	)
+	if err != nil {
+		log.Criticalf("Unable to setup logger: %s", err)
+		return
+	}
+
+	if !config.Datadog.IsSet("api_key") {
+		log.Critical("no API key configured, exiting")
+		return
+	}
+
+	// Setup healthcheck port
+	var healthPort = config.Datadog.GetInt("health_port")
+	if healthPort > 0 {
+		err = healthprobe.Serve(mainCtx, healthPort)
+		if err != nil {
+			err = log.Errorf("Error starting health port, exiting: %v", err)
+			return
+		}
+		log.Debugf("Health check listening on port %d", healthPort)
+	}
+
+	// setup the forwarder
+	keysPerDomain, err := config.GetMultipleEndpoints()
+	if err != nil {
+		log.Error("Misconfiguration of agent endpoints: ", err)
+	}
+	f := forwarder.NewDefaultForwarder(keysPerDomain)
+	f.Start()
+	s := serializer.NewSerializer(f)
+
+	hname, err := util.GetHostname()
+	if err != nil {
+		log.Warnf("Error getting hostname: %s", err)
+		hname = ""
+	}
+	log.Debugf("Using hostname: %s", hname)
+
+	// setup the metadata collector
+	metaScheduler = metadata.NewScheduler(s)
+	if err = metadata.SetupMetadataCollection(metaScheduler, []string{"host"}); err != nil {
+		metaScheduler.Stop()
+		return
+	}
+
+	if config.Datadog.GetBool("inventories_enabled") {
+		if err = metadata.SetupInventories(metaScheduler, nil, nil); err != nil {
+			return
+		}
+	}
+
+	// container tagging initialisation if origin detection is on
+	if config.Datadog.GetBool("dogstatsd_origin_detection") {
+		tagger.Init()
+	}
+
+	metricSamplePool := metrics.NewMetricSamplePool(32)
+	aggregatorInstance := aggregator.InitAggregator(s, metricSamplePool, hname, "agent")
+	sampleC, eventC, serviceCheckC := aggregatorInstance.GetBufferedChannels()
+	statsd, err = dogstatsd.NewServer(metricSamplePool, sampleC, eventC, serviceCheckC)
+	if err != nil {
+		log.Criticalf("Unable to start dogstatsd: %s", err)
+		return
+	}
+	return
+}
+
+func stopAgent(ctx context.Context, cancel context.CancelFunc) {
+	// retrieve the agent health before stopping the components
+	// GetStatusNonBlocking has a 100ms timeout to avoid blocking
+	health, err := health.GetStatusNonBlocking()
+	if err != nil {
+		log.Warnf("Dogstatsd health unknown: %s", err)
+	} else if len(health.Unhealthy) > 0 {
+		log.Warnf("Some components were unhealthy: %v", health.Unhealthy)
+	}
+
+	// gracefully shut down any component
+	cancel()
+
+	metaScheduler.Stop()
+	statsd.Stop()
+	log.Info("See ya!")
+	log.Flush()
+	return
+}
