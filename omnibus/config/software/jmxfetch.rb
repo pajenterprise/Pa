9b57931fe5cb3faca6a54cde771d55def2d17f0d
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:09:31 -0400

    Initialize devlin-yaml-examples

---
+++ omnibus/config/software/jmxfetch.rb
@@ -0,0 +1,30 @@
+# Unless explicitly stated otherwise all files in this repository are licensed
+# under the Apache License Version 2.0.
+# This product includes software developed at Datadog (https:#www.datadoghq.com/).
+# Copyright 2016-2019 Datadog, Inc.
+
+name "jmxfetch"
+
+jmxfetch_version = ENV['JMXFETCH_VERSION']
+jmxfetch_hash = ENV['JMXFETCH_HASH']
+
+if jmxfetch_version.nil? || jmxfetch_version.empty?
+  jmxfetch_version = '0.24.1'
+  jmxfetch_hash = "244cdcec15efb9fc9923a868787d3e6a2dad95e169dbee4cb7fe6d122d5109cf"
+end
+
+default_version jmxfetch_version
+source sha256: jmxfetch_hash
+
+source :url => "https://dl.bintray.com/datadog/datadog-maven/com/datadoghq/jmxfetch/#{version}/jmxfetch-#{version}-jar-with-dependencies.jar"
+
+jar_dir = "#{install_dir}/bin/agent/dist/jmx"
+
+relative_path "jmxfetch"
+
+build do
+  ship_license "https://raw.githubusercontent.com/DataDog/jmxfetch/master/LICENSE"
+  mkdir jar_dir
+  copy "jmxfetch-#{jmxfetch_version}-jar-with-dependencies.jar", jar_dir
+  block { File.chmod(0644, "#{jar_dir}/jmxfetch-#{jmxfetch_version}-jar-with-dependencies.jar") }
+end
