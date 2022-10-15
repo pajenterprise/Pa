f2e4c03347686d92a7f9032d8c1bde36ce81597b
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:03:59 -0400

    Initialize mtremsal-patch-typo-1

---
+++ pkg/util/kubernetes/apiserver/diagnosis.go
@@ -0,0 +1,27 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2017 Datadog, Inc.
+
+// +build kubeapiserver
+
+package apiserver
+
+import (
+	log "github.com/cihub/seelog"
+
+	"github.com/DataDog/datadog-agent/pkg/diagnose/diagnosis"
+)
+
+func init() {
+	diagnosis.Register("Kubernetes API Server availability", diagnose)
+}
+
+// diagnose the API server availability
+func diagnose() error {
+	_, err := GetAPIClient()
+	if err != nil {
+		log.Error(err)
+	}
+	return err
+}
