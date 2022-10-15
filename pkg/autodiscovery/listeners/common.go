d9960292b2e8eca09af789106f77f2e7aa209161
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:33:58 -0400

    Initialize remeh/go-1.13.6-clangs-paths

---
+++ pkg/autodiscovery/listeners/common.go
@@ -0,0 +1,63 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2017-2020 Datadog, Inc.
+
+package listeners
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/DataDog/datadog-agent/pkg/util/containers"
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+)
+
+const (
+	newIdentifierLabel         = "com.datadoghq.ad.check.id"
+	legacyIdentifierLabel      = "com.datadoghq.sd.check.id"
+	dockerADTemplateCheckNames = "com.datadoghq.ad.check_names"
+)
+
+// ComputeContainerServiceIDs takes an entity name, an image (resolved to an actual name) and labels
+// and computes the service IDs for this container service.
+func ComputeContainerServiceIDs(entity string, image string, labels map[string]string) []string {
+	// ID override label
+	if l, found := labels[newIdentifierLabel]; found {
+		return []string{l}
+	}
+	if l, found := labels[legacyIdentifierLabel]; found {
+		log.Warnf("found legacy %s label for %s, please use the new name %s",
+			legacyIdentifierLabel, entity, newIdentifierLabel)
+		return []string{l}
+	}
+
+	ids := []string{entity}
+
+	// Add Image names (long then short if different)
+	long, short, _, err := containers.SplitImageName(image)
+	if err != nil {
+		log.Warnf("error while spliting image name: %s", err)
+	}
+	if len(long) > 0 {
+		ids = append(ids, long)
+	}
+	if len(short) > 0 && short != long {
+		ids = append(ids, short)
+	}
+	return ids
+}
+
+// getCheckNamesFromLabels unmarshals the json string of check names
+// defined in docker labels and returns a slice of check names
+func getCheckNamesFromLabels(labels map[string]string) ([]string, error) {
+	if checkLabels, found := labels[dockerADTemplateCheckNames]; found {
+		checkNames := []string{}
+		err := json.Unmarshal([]byte(checkLabels), &checkNames)
+		if err != nil {
+			return nil, fmt.Errorf("Cannot parse check names: %v", err)
+		}
+		return checkNames, nil
+	}
+	return nil, nil
+}
