abc8ececcdc391370c2cea509bd4e9676d3e1722
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:23:22 -0400

    Initialize ecs-resource-tags

---
+++ pkg/logs/processor/processor.go
@@ -0,0 +1,91 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2020 Datadog, Inc.
+
+package processor
+
+import (
+	"github.com/DataDog/datadog-agent/pkg/util/log"
+
+	"github.com/DataDog/datadog-agent/pkg/logs/config"
+	"github.com/DataDog/datadog-agent/pkg/logs/message"
+	"github.com/DataDog/datadog-agent/pkg/logs/metrics"
+)
+
+// A Processor updates messages from an inputChan and pushes
+// in an outputChan.
+type Processor struct {
+	inputChan       chan *message.Message
+	outputChan      chan *message.Message
+	processingRules []*config.ProcessingRule
+	encoder         Encoder
+	done            chan struct{}
+}
+
+// New returns an initialized Processor.
+func New(inputChan, outputChan chan *message.Message, processingRules []*config.ProcessingRule, encoder Encoder) *Processor {
+	return &Processor{
+		inputChan:       inputChan,
+		outputChan:      outputChan,
+		processingRules: processingRules,
+		encoder:         encoder,
+		done:            make(chan struct{}),
+	}
+}
+
+// Start starts the Processor.
+func (p *Processor) Start() {
+	go p.run()
+}
+
+// Stop stops the Processor,
+// this call blocks until inputChan is flushed
+func (p *Processor) Stop() {
+	close(p.inputChan)
+	<-p.done
+}
+
+// run starts the processing of the inputChan
+func (p *Processor) run() {
+	defer func() {
+		p.done <- struct{}{}
+	}()
+	for msg := range p.inputChan {
+		metrics.LogsDecoded.Add(1)
+		if shouldProcess, redactedMsg := p.applyRedactingRules(msg); shouldProcess {
+			metrics.LogsProcessed.Add(1)
+
+			// Encode the message to its final format
+			content, err := p.encoder.Encode(msg, redactedMsg)
+			if err != nil {
+				log.Error("unable to encode msg ", err)
+				continue
+			}
+			msg.Content = content
+			p.outputChan <- msg
+		}
+	}
+}
+
+// applyRedactingRules returns given a message if we should process it or not,
+// and a copy of the message with some fields redacted, depending on config
+func (p *Processor) applyRedactingRules(msg *message.Message) (bool, []byte) {
+	content := msg.Content
+	rules := append(p.processingRules, msg.Origin.LogSource.Config.ProcessingRules...)
+	for _, rule := range rules {
+		switch rule.Type {
+		case config.ExcludeAtMatch:
+			if rule.Regex.Match(content) {
+				return false, nil
+			}
+		case config.IncludeAtMatch:
+			if !rule.Regex.Match(content) {
+				return false, nil
+			}
+		case config.MaskSequences:
+			content = rule.Regex.ReplaceAll(content, rule.Placeholder)
+		}
+	}
+	return true, content
+}
