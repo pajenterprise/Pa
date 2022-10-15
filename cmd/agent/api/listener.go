8fc1cc8ff97bd33b4de1386c1bf07f43a9358add
Patrick A Jonas <41257547+pajenterprisesllc@users.noreply.github.com>
2022-10-14 21:18:18 -0400

    Initialize arbll/6.13.x-configurable-ipc-bind

---
+++ cmd/agent/api/listener.go
@@ -0,0 +1,18 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2016-2019 Datadog, Inc.
+
+package api
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/DataDog/datadog-agent/pkg/config"
+)
+
+// getListener returns a listening connection
+func getListener() (net.Listener, error) {
+	return net.Listen("tcp", fmt.Sprintf("%v:%v", config.Datadog.GetString("bind_ipc"), config.Datadog.GetInt("cmd_port")))
+}
